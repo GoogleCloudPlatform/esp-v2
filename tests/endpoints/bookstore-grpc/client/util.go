d75d0b0fe04ff7db850e74dbf43d1b3471ff22cb
Kyu Chang <kyuc@google.com>
2019-01-09 17:02:22 +0000

    Implement DecodeGrpcWebResponseBody for integratoin tests
    Change-Id: Iec4108d2d60cbdc147c3c80db3329a2fa29811b0

--- tests/endpoints/bookstore-grpc/client/util.go
+++ tests/endpoints/bookstore-grpc/client/util.go
@@ -1,17 +1,29 @@
+// Various utility functions for testing gRPC-Web.
+//
+// Implementations are based on the protocol specificication.
+// For details refer to:
+// https://github.com/grpc/grpc/blob/master/doc/PROTOCOL-WEB.md#protocol-differences-vs-grpc-over-http2
+
 package client
 
 import (
 	"bytes"
 	"encoding/base64"
 	"encoding/binary"
+	"errors"
 	"io"
+	"io/ioutil"
+	"strings"
 
 	"github.com/golang/protobuf/proto"
 )
 
+// GRPCWebTrailer represents key-value pairs in gRPC-Web trailer.
+type GRPCWebTrailer map[string]string
+
 // EncodeGrpcWebRequestBody returns an encoded reader given a request message.
-func EncodeGrpcWebRequestBody(msg proto.Message) io.Reader {
-	data, _ := proto.Marshal(msg)
+func EncodeGrpcWebRequestBody(message proto.Message) io.Reader {
+	data, _ := proto.Marshal(message)
 	lengthPrefix := []byte{0, 0, 0, 0, 0}
 	binary.BigEndian.PutUint32(lengthPrefix[1:], uint32(len(data)))
 	var buf bytes.Buffer
@@ -23,4 +35,74 @@ func EncodeGrpcWebRequestBody(msg proto.Message) io.Reader {
 	return bytes.NewReader(b)
 }
 
-// TODO(kyuc): add DecodeGrpcWebResponseBody()
+// DecodeGrpcWebResponseBody returns a decoded message and a trailer given a request body.
+func DecodeGrpcWebResponseBody(body io.Reader) ([]byte, GRPCWebTrailer, error) {
+	content, err := ioutil.ReadAll(body)
+	if err != nil {
+		return nil, nil, err
+	}
+
+	decodedBytes := base64DecodeBytes(content)
+	data := bytes.NewReader(decodedBytes)
+
+	// Reading the response message.
+	_, message, err := readPayloadBytes(data)
+	if err != nil {
+		return nil, nil, err
+	}
+
+	// Reading the trailer.
+	lengthPrefix, trailerBytes, err := readPayloadBytes(data)
+	trailer := make(GRPCWebTrailer)
+	if !isTrailer(lengthPrefix[0]) {
+		return nil, nil, errors.New("malformed gRPC-Web response")
+	}
+
+	// key-value pairs are delimited by \r\n
+	for _, keyValue := range strings.Split(string(trailerBytes), "\r\n") {
+		kv := strings.Split(strings.TrimSpace(keyValue), ":")
+		if len(kv) == 2 {
+			trailer[kv[0]] = kv[1]
+		}
+	}
+
+	return message, trailer, nil
+}
+
+// base64DecodeBytes decodes base 64 encoded byte slice.
+func base64DecodeBytes(encodedBytes []byte) []byte {
+	decodedLen := base64.StdEncoding.DecodedLen(len(encodedBytes))
+	decodedBytes := make([]byte, decodedLen)
+	numBytesRead := 0
+	numBytesWritten := 0
+	for numBytesRead+3 < len(encodedBytes) {
+		n, _ := base64.StdEncoding.Decode(decodedBytes[numBytesWritten:], encodedBytes[numBytesRead:])
+		numBytesWritten += n
+		numBytesRead += base64.StdEncoding.EncodedLen(n)
+	}
+	return decodedBytes[:numBytesWritten]
+}
+
+// isTrailer returns true if the gRPCFrameByte's most significant bit is 1.
+func isTrailer(gRPCFrameByte byte) bool {
+	return gRPCFrameByte&(1<<7) == (1 << 7)
+}
+
+// readPayloadBytes returns the next payload bytes in the data.
+func readPayloadBytes(data io.Reader) ([]byte, []byte, error) {
+	lengthPrefix := []byte{0, 0, 0, 0, 0}
+	readCount, err := data.Read(lengthPrefix)
+
+	if err != nil {
+		return nil, nil, err
+	}
+
+	if readCount != 5 {
+		return nil, nil, errors.New("malformed data: not enough data to read length prefix")
+	}
+
+	payloadLength := binary.BigEndian.Uint32(lengthPrefix[1:])
+	trailerBytes := make([]byte, payloadLength)
+	readCount, err = data.Read(trailerBytes)
+	return lengthPrefix, trailerBytes, nil
+}
--- tests/endpoints/bookstore-grpc/client/util_test.go
+++ tests/endpoints/bookstore-grpc/client/util_test.go
@@ -3,11 +3,13 @@ package client
 import (
 	"bytes"
 	"io"
+	"strings"
 	"testing"
 
 	bspb "cloudesf.googlesource.com/gcpproxy/tests/endpoints/bookstore-grpc/proto"
 	"github.com/gogo/protobuf/types"
 	"github.com/golang/protobuf/proto"
+	"reflect"
 )
 
 func readerToString(r io.Reader) string {
@@ -48,6 +50,79 @@ func TestEncodeGrpcWebRequestBody(t *testing.T) {
 		encodedReqBody := readerToString(r)
 		if encodedReqBody != tc.expectedReqBody {
 			t.Errorf("Actual: %v. Expected: %v", encodedReqBody, tc.expectedReqBody)
+		}
+	}
+}
+
+func TestDecodeGrpcWebResponseBody(t *testing.T) {
+	successTrailer := GRPCWebTrailer{"grpc-message": "OK", "grpc-status": "0"}
+	testCases := []struct {
+		desc string
+		// base64-encoded response body received by a gRPC-Web client.
+		encodedRespBody string
+		actualMsg       proto.Message
+		expectedMsg     proto.Message
+		expectedTrailer GRPCWebTrailer
+	}{
+		{
+			desc:            "ListShelves",
+			encodedRespBody: "AAAAABwKDgh7EgpTaGFrc3BlYXJlCgoIfBIGSGFtbGV0gAAAACBncnBjLXN0YXR1czowDQpncnBjLW1lc3NhZ2U6T0sNCg==",
+			actualMsg:       &bspb.ListShelvesResponse{},
+			expectedMsg: &bspb.ListShelvesResponse{
+				Shelves: []*bspb.Shelf{
+					&bspb.Shelf{
+						Id:    123,
+						Theme: "Shakspeare",
+					},
+					&bspb.Shelf{
+						Id:    124,
+						Theme: "Hamlet",
+					},
+				},
+			},
+			expectedTrailer: successTrailer,
+		},
+		{
+			desc:            "GetShelf",
+			encodedRespBody: "AAAAABEIexINVW5rbm93biBTaGVsZg==gAAAACBncnBjLXN0YXR1czowDQpncnBjLW1lc3NhZ2U6T0sNCg==",
+			actualMsg:       &bspb.Shelf{},
+			expectedMsg: &bspb.Shelf{
+				Id:    123,
+				Theme: "Unknown Shelf",
+			},
+			expectedTrailer: successTrailer,
+		},
+		{
+			desc:            "CreateBook",
+			encodedRespBody: "AAAAAAwIexoITmV3IEJvb2s=gAAAACBncnBjLXN0YXR1czowDQpncnBjLW1lc3NhZ2U6T0sNCg==",
+			actualMsg:       &bspb.Book{},
+			expectedMsg: &bspb.Book{
+				Id:    123,
+				Title: "New Book",
+			},
+			expectedTrailer: successTrailer,
+		},
+	}
+
+	for _, tc := range testCases {
+		binaryMsg, trailer, err := DecodeGrpcWebResponseBody(strings.NewReader(tc.encodedRespBody))
+		if err != nil {
+			t.Errorf("%s failed with error: %v,", tc.desc, err)
+			continue
+		}
+
+		err = proto.Unmarshal(binaryMsg, tc.actualMsg)
+		if err != nil {
+			t.Errorf("%s failed with error: %v,", tc.desc, err)
+			continue
+		}
+
+		if !proto.Equal(tc.actualMsg, tc.expectedMsg) {
+			t.Errorf("%s failed:\nActual:%v\nExpected:%v", tc.desc, tc.actualMsg, tc.expectedMsg)
+		}
+
+		if !reflect.DeepEqual(trailer, tc.expectedTrailer) {
+			t.Errorf("%s failed:\nActual:%v\nExpected:%v", tc.desc, trailer, tc.expectedTrailer)
 		}
 	}
 }
