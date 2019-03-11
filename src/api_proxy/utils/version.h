1bbf2d18dfef7f4567152cb95bc687cb772e8f0f
Connor Douthat <cdouthat@google.com>
2019-03-11 14:27:04 -0600

    Add version file and util
    http: //b/123950588
    Change-Id: Ia44e75bad3c50a536d6886ef4f61b5aa522057d4

---
+++ src/api_proxy/utils/version.h
@@ -0,0 +1,46 @@
+// Copyright 2019 Google Cloud Platform Proxy Authors
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+#ifndef API_PROXY_UTILS_VERSION_H_
+#define API_PROXY_UTILS_VERSION_H_
+
+#include <string>
+
+namespace google {
+namespace api_proxy {
+namespace utils {
+
+// Provides a singleton for getting the API Proxy version.
+class Version final {
+ public:
+  // Gets the singleton instance.
+  static Version& instance();
+
+  // Gets the version, which will be populated from the version file by default.
+  const std::string& get() const { return version_; }
+
+  // Sets the version. Only use for tests.
+  void set(const std::string& v) { version_ = v; }
+
+ private:
+  std::string version_;
+
+  // Constructs with a default version. Private to require singleton use.
+  Version(const std::string& v) { version_ = v; }
+};
+
+}  // namespace utils
+}  // namespace api_proxy
+}  // namespace google
+
+#endif  // API_PROXY_UTILS_VERSION_H_
