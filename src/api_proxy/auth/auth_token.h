0edbb75e99a083df265ccad75ba43d2192971054
Xuyang Tao <taoxuy@google.com>
2019-07-08 17:10:38 +0000

    [Migration] Migrate ESP's service account related auth lib.
    Change-Id: Id7bd0d5f6f4cbe1d9ff66458a25d9c25012e10de

---
+++ src/api_proxy/auth/auth_token.h
@@ -0,0 +1,35 @@
+/* Copyright 2019 Google Cloud Platform Proxy Authors
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *    http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+#ifndef API_MANAGER_AUTH_LIB_AUTH_TOKEN_H_
+#define API_MANAGER_AUTH_LIB_AUTH_TOKEN_H_
+
+#include <stddef.h>
+
+namespace google {
+namespace api_proxy {
+namespace auth {
+
+// Parse a json secret and generate auth_token
+// Returned pointer need to be freed by grpc_free
+char *get_auth_token(const char *json_secret, const char *audience);
+
+// Free a buffer allocated by gRPC library.
+void grpc_free(char *token);
+
+}  // namespace auth
+}  // namespace api_proxy
+}  // namespace google
+
+#endif  // API_MANAGER_AUTH_LIB_AUTH_TOKEN_H_
