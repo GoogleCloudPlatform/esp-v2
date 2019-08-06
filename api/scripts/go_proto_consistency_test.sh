f216aa88268d42758ca2460f6e18cd5aeadd3348
Teju Nareddy <nareddyt@google.com>
2019-08-05 17:09:03 -0700

    Remove dependency on go-grpc-library
    This dependency complicates the import process into google3.
    Instead, just depend on go-proto-library.
    
    Also rename targets to follow google3 convention.
    Otherwise, google3 throws errors.
    
    Change-Id: I238ddbef49b2270708f959986ef3c8908a91f0cf

--- api/scripts/go_proto_consistency_test.sh
+++ api/scripts/go_proto_consistency_test.sh
@@ -18,8 +18,8 @@
 set -eo pipefail
 
 # HTTP filter common
-bazel build //api/envoy/http/common:base_proto_go_proto
-diff bazel-bin/api/envoy/http/common/*/base_proto_go_proto%/cloudesf.googlesource.com/gcpproxy/src/go/proto/api/envoy/http/common src/go/proto/api/envoy/http/common
+bazel build //api/envoy/http/common:base_go_proto
+diff bazel-bin/api/envoy/http/common/*/base_go_proto%/cloudesf.googlesource.com/gcpproxy/src/go/proto/api/envoy/http/common src/go/proto/api/envoy/http/common
 # HTTP filter service_control
-bazel build //api/envoy/http/service_control:config_proto_go_proto
-diff bazel-bin/api/envoy/http/service_control/*/config_proto_go_proto%/cloudesf.googlesource.com/gcpproxy/src/go/proto/api/envoy/http/service_control src/go/proto/api/envoy/http/service_control
+bazel build //api/envoy/http/service_control:config_go_proto
+diff bazel-bin/api/envoy/http/service_control/*/config_go_proto%/cloudesf.googlesource.com/gcpproxy/src/go/proto/api/envoy/http/service_control src/go/proto/api/envoy/http/service_control
--- api/scripts/go_proto_gen.sh
+++ api/scripts/go_proto_gen.sh
@@ -24,25 +24,25 @@ rm -rf vendor/github.com/census-instrumentation/opencensus-proto/gen-go
 
 #TODO(bochun): probably we can programatically generate these.
 # HTTP filter common
-bazel build //api/envoy/http/common:base_proto_go_proto
+bazel build //api/envoy/http/common:base_go_proto
 mkdir -p src/go/proto/api/envoy/http/common
-cp -f bazel-bin/api/envoy/http/common/*/base_proto_go_proto%/cloudesf.googlesource.com/gcpproxy/src/go/proto/api/envoy/http/common/* src/go/proto/api/envoy/http/common
+cp -f bazel-bin/api/envoy/http/common/*/base_go_proto%/cloudesf.googlesource.com/gcpproxy/src/go/proto/api/envoy/http/common/* src/go/proto/api/envoy/http/common
 # HTTP filter service_control
-bazel build //api/envoy/http/service_control:config_proto_go_proto
+bazel build //api/envoy/http/service_control:config_go_proto
 mkdir -p src/go/proto/api/envoy/http/service_control
-cp -f bazel-bin/api/envoy/http/service_control/*/config_proto_go_proto%/cloudesf.googlesource.com/gcpproxy/src/go/proto/api/envoy/http/service_control/* src/go/proto/api/envoy/http/service_control
+cp -f bazel-bin/api/envoy/http/service_control/*/config_go_proto%/cloudesf.googlesource.com/gcpproxy/src/go/proto/api/envoy/http/service_control/* src/go/proto/api/envoy/http/service_control
 # HTTP filter path_matcher
-bazel build //api/envoy/http/path_matcher:config_proto_go_proto
+bazel build //api/envoy/http/path_matcher:config_go_proto
 mkdir -p src/go/proto/api/envoy/http/path_matcher
-cp -f bazel-bin/api/envoy/http/path_matcher/*/config_proto_go_proto%/cloudesf.googlesource.com/gcpproxy/src/go/proto/api/envoy/http/path_matcher/* src/go/proto/api/envoy/http/path_matcher
+cp -f bazel-bin/api/envoy/http/path_matcher/*/config_go_proto%/cloudesf.googlesource.com/gcpproxy/src/go/proto/api/envoy/http/path_matcher/* src/go/proto/api/envoy/http/path_matcher
 # HTTP filter backend_auth
-bazel build //api/envoy/http/backend_auth:config_proto_go_proto
+bazel build //api/envoy/http/backend_auth:config_go_proto
 mkdir -p src/go/proto/api/envoy/http/backend_auth
-cp -f bazel-bin/api/envoy/http/backend_auth/*/config_proto_go_proto%/cloudesf.googlesource.com/gcpproxy/src/go/proto/api/envoy/http/backend_auth/* src/go/proto/api/envoy/http/backend_auth
+cp -f bazel-bin/api/envoy/http/backend_auth/*/config_go_proto%/cloudesf.googlesource.com/gcpproxy/src/go/proto/api/envoy/http/backend_auth/* src/go/proto/api/envoy/http/backend_auth
 # HTTP filter backend_routing
-bazel build //api/envoy/http/backend_routing:config_proto_go_proto
+bazel build //api/envoy/http/backend_routing:config_go_proto
 mkdir -p src/go/proto/api/envoy/http/backend_routing
-cp -f bazel-bin/api/envoy/http/backend_routing/*/config_proto_go_proto%/cloudesf.googlesource.com/gcpproxy/src/go/proto/api/envoy/http/backend_routing/* src/go/proto/api/envoy/http/backend_routing
+cp -f bazel-bin/api/envoy/http/backend_routing/*/config_go_proto%/cloudesf.googlesource.com/gcpproxy/src/go/proto/api/envoy/http/backend_routing/* src/go/proto/api/envoy/http/backend_routing
 
 # envoy protos
 bazel build @envoy_api//envoy/...
