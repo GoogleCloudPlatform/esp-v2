9e6eccb0eaad44b601a8fb061eb23e492f8cb23d
Teju Nareddy <nareddyt@google.com>
2019-11-05 11:16:29 -0800

    b/143636707 #2: Move golang/x/oauth2 code to third_party
    Required in order to import into google3 and release on Github.
    
    Change-Id: I20634456c12ad6930cd04fd8800bdb73e4651024
    Signed-off-by: Teju Nareddy <nareddyt@google.com>

--- tests/endpoints/echo/client/client.go
+++ tests/endpoints/echo/client/client.go
@@ -16,11 +16,7 @@ package client
 
 import (
 	"bytes"
-	"crypto/rsa"
-	"crypto/x509"
 	"encoding/json"
-	"encoding/pem"
-	"errors"
 	"fmt"
 	"io/ioutil"
 	"net/http"
@@ -29,6 +25,8 @@ import (
 	"github.com/GoogleCloudPlatform/api-proxy/tests/env/testdata"
 	"golang.org/x/oauth2/google"
 	"golang.org/x/oauth2/jws"
+
+	oauth2_internal "github.com/GoogleCloudPlatform/api-proxy/third_party/golang-internal"
 )
 
 // DoGet performs a Get request to a specified url
@@ -102,7 +100,7 @@ func DoJWT(host, method, path, apiKey, serviceAccount, token string) ([]byte, er
 		if err != nil {
 			return nil, fmt.Errorf("Could not parse service account JSON: %v", err)
 		}
-		rsaKey, err := parseKey(conf.PrivateKey)
+		rsaKey, err := oauth2_internal.ParseKey(conf.PrivateKey)
 		if err != nil {
 			return nil, fmt.Errorf("Could not get RSA key: %v", err)
 		}
@@ -198,57 +196,4 @@ func DoCorsPreflightRequest(url, origin, requestMethod, requestHeader, referer s
 	}
 	defer resp.Body.Close()
 	return resp.Header, nil
-}
-
-// The following code is copied from golang.org/x/oauth2/internal
-// Copyright (c) 2009 The oauth2 Authors. All rights reserved.
-
-// Redistribution and use in source and binary forms, with or without
-// modification, are permitted provided that the following conditions are
-// met:
-
-//   * Redistributions of source code must retain the above copyright
-//notice, this list of conditions and the following disclaimer.
-//   * Redistributions in binary form must reproduce the above
-// copyright notice, this list of conditions and the following disclaimer
-// in the documentation and/or other materials provided with the
-// distribution.
-//    * Neither the name of Google Inc. nor the names of its
-// contributors may be used to endorse or promote products derived from
-// this software without specific prior written permission.
-//
-// THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS
-// "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT
-// LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR
-// A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT
-// OWNER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL,
-// SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT
-// LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE,
-// DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY
-// THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
-// (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE
-// OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
-
-// parseKey converts the binary contents of a private key file
-// to an *rsa.PrivateKey. It detects whether the private key is in a
-// PEM container or not. If so, it extracts the the private key
-// from PEM container before conversion. It only supports PEM
-// containers with no passphrase.
-func parseKey(key []byte) (*rsa.PrivateKey, error) {
-	block, _ := pem.Decode(key)
-	if block != nil {
-		key = block.Bytes
-	}
-	parsedKey, err := x509.ParsePKCS8PrivateKey(key)
-	if err != nil {
-		parsedKey, err = x509.ParsePKCS1PrivateKey(key)
-		if err != nil {
-			return nil, fmt.Errorf("private key should be a PEM or plain PKSC1 or PKCS8; parse error: %v", err)
-		}
-	}
-	parsed, ok := parsedKey.(*rsa.PrivateKey)
-	if !ok {
-		return nil, errors.New("private key is invalid")
-	}
-	return parsed, nil
 }
