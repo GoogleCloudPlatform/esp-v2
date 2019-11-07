916ab3c3b9554bc6c48e94e484cd4e9883cfd544
Xuyang Tao <taoxuy@google.com>
2019-11-07 08:53:14 -0800

    chaneg cloudesf to apiproxy in release related scripts
    Change-Id: Icad97973aca81e41dcaa41599eda54d7b6fcf94d

--- scripts/release/release-show.sh
+++ scripts/release/release-show.sh
@@ -26,7 +26,7 @@ function usage() {
 
 Usage: ${BASH_SOURCE[0]} [-r <DIRECT_REPO>]
 
-This script will show all release CloudESF image tags
+This script will show all release Api Proxy image tags
 
 END_USAGE
   exit 1
