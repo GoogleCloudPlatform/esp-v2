52f32082dd02c3d9a7aa61995ea97db000dc115e
Teju Nareddy <nareddyt@google.com>
2019-12-20 15:40:12 -0800

    b/146674995: Fix use of files in unit tests
    - Allow simplier copybara transformation when copying code
    - Also run clang-format
    
    Signed-off-by: Teju Nareddy <nareddyt@google.com>
    Change-Id: I411cc6ff9b454da2f5fdaf6218924423d2670360

--- src/api_proxy/path_matcher/http_template_fuzz_test.cc
+++ src/api_proxy/path_matcher/http_template_fuzz_test.cc
@@ -1,6 +1,6 @@
+#include "src/api_proxy/path_matcher/http_template.h"
 #include "test/fuzz/fuzz_runner.h"
 #include "test/fuzz/utility.h"
-#include "src/api_proxy/path_matcher/http_template.h"
 
 #include "tests/fuzz/structured_inputs/http_template.pb.validate.h"
 
@@ -8,16 +8,13 @@ namespace google {
 namespace api_proxy {
 namespace path_matcher {
 namespace fuzz {
-DEFINE_PROTO_FUZZER(
-    const tests::fuzz::protos::HttpTemplateInput& input) {
-
+DEFINE_PROTO_FUZZER(const tests::fuzz::protos::HttpTemplateInput& input) {
   for (const auto& path : input.paths()) {
     HttpTemplate::Parse(path);
   }
-
 }
 
-}  //fuzz
+}  // namespace fuzz
 }  // namespace path_matcher
 }  // namespace api_proxy
 }  // namespace google
