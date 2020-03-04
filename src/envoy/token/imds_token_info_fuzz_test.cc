e4459d12e21f754f4a7b0b6ef13bb8e8d6d8185a
Teju Nareddy <nareddyt@google.com>
2020-03-03 17:59:25 -0800

    Improve performance and coverage of fuzz tests for `TokenInfo` (#42)
    - Now that TokenInfo is decoupled from TokenSubscriber, switch the fuzz tests.
    - Add tests for IamTokenInfo and ImdsTokenInfo.
    - TokenSubscriber already has high test coverage from unit tests. No need to fuzz test it.
    
    Signed-off-by: Teju Nareddy <nareddyt@google.com>

---
+++ src/envoy/token/imds_token_info_fuzz_test.cc
@@ -0,0 +1,48 @@
+// Copyright 2020 Google LLC
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+#include "test/fuzz/fuzz_runner.h"
+#include "test/fuzz/utility.h"
+
+#include "src/envoy/token/imds_token_info.h"
+#include "tests/fuzz/structured_inputs/imds_token_info.pb.validate.h"
+
+namespace Envoy {
+namespace Extensions {
+namespace Token {
+namespace Test {
+
+DEFINE_PROTO_FUZZER(const tests::fuzz::protos::ImdsTokenInfoInput& input) {
+  ENVOY_LOG_MISC(trace, "{}", input.DebugString());
+
+  try {
+    TestUtility::validate(input);
+
+    ImdsTokenInfo token_info;
+
+    // Call functions under test.
+    TokenResult ret;
+    (void)token_info.prepareRequest(input.token_url());
+    (void)token_info.parseAccessToken(input.resp_body(), &ret);
+    (void)token_info.parseIdentityToken(input.resp_body(), &ret);
+
+  } catch (const ProtoValidationException& e) {
+    ENVOY_LOG_MISC(debug, "Controlled proto validation failure: {}", e.what());
+  }
+}
+
+}  // namespace Test
+}  // namespace Token
+}  // namespace Extensions
+}  // namespace Envoy
