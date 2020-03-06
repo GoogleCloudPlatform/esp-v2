65a3d65de9b408653fe5dcaee99657ec92bd6ae2
Teju Nareddy <nareddyt@google.com>
2020-03-06 10:44:11 -0800

    Review comments
    Signed-off-by: Teju Nareddy <nareddyt@google.com>

--- src/envoy/http/service_control/filter_fuzz_test.cc
+++ src/envoy/http/service_control/filter_fuzz_test.cc
@@ -32,44 +32,67 @@ namespace Fuzz {
 
 void doTest(ServiceControlFilter& filter, TestStreamInfo& stream_info,
             const tests::fuzz::protos::ServiceControlFilterInput& input) {
-  // Setup downstream request.
+  // Decode headers.
+  bool end_stream = false;
   auto downstream_headers =
       Envoy::Fuzz::fromHeaders<Envoy::Http::TestRequestHeaderMapImpl>(
           input.downstream_request().headers());
-  auto downstream_body =
-      Buffer::OwnedImpl(input.downstream_request().data().Get(0));
+  if (input.downstream_request().data().size() == 0 &&
+      !input.downstream_request().has_trailers()) {
+    end_stream = true;
+  }
+  filter.decodeHeaders(downstream_headers, end_stream);
+
+  // Decode body (if needed).
+  for (int i = 0; i < input.downstream_request().data().size(); i++) {
+    if (i == input.downstream_request().data().size() - 1 &&
+        !input.downstream_request().has_trailers()) {
+      end_stream = true;
+    }
+    Buffer::OwnedImpl buffer(input.downstream_request().data().Get(i));
+    filter.decodeData(buffer, end_stream);
+  }
+
+  // Decode trailers (if needed).
   auto downstream_trailers =
       Envoy::Fuzz::fromHeaders<Envoy::Http::TestRequestTrailerMapImpl>(
           input.downstream_request().trailers());
-  // TODO(b/146671523): Uncomment this when implemented upstream.
-  // stream_info.addBytesReceived(downstream_body.length());
+  if (input.downstream_request().has_trailers()) {
+    filter.decodeTrailers(downstream_trailers);
+  }
 
-  // Downstream functions under test.
-  filter.decodeHeaders(downstream_headers, false);
-  filter.decodeData(downstream_body, false);
-  filter.decodeData(downstream_body, true);
-  filter.decodeTrailers(downstream_trailers);
-
-  // Setup upstream response.
+  // Encode headers.
   auto upstream_headers =
       Envoy::Fuzz::fromHeaders<Envoy::Http::TestResponseHeaderMapImpl>(
           input.upstream_response().headers());
-  auto upstream_body =
-      Buffer::OwnedImpl(input.upstream_response().data().Get(0));
+  if (input.upstream_response().data().size() == 0 &&
+      !input.upstream_response().has_trailers()) {
+    end_stream = true;
+  }
+  filter.encodeHeaders(upstream_headers, end_stream);
+
+  // Encode body (if needed).
+  end_stream = false;
+  for (int i = 0; i < input.upstream_response().data().size(); i++) {
+    if (i == input.upstream_response().data().size() - 1 &&
+        !input.upstream_response().has_trailers()) {
+      end_stream = true;
+    }
+    Buffer::OwnedImpl buffer(input.upstream_response().data().Get(i));
+    filter.encodeData(buffer, end_stream);
+  }
+
+  // Encode trailers (if needed).
   auto upstream_trailers =
       Envoy::Fuzz::fromHeaders<Envoy::Http::TestResponseTrailerMapImpl>(
           input.upstream_response().trailers());
-  // TODO(b/146671523): Uncomment this when implemented upstream.
-  // stream_info.addBytesSent(upstream_body.length());
+  if (input.upstream_response().has_trailers()) {
+    filter.encodeTrailers(upstream_trailers);
+  }
 
-  // Upstream functions under test.
-  filter.encodeHeaders(upstream_headers, false);
-  filter.encodeData(upstream_body, false);
-  filter.encodeData(upstream_body, true);
-  filter.encodeTrailers(upstream_trailers);
-
-  // Report function under test.
-  filter.log(&downstream_headers, &upstream_headers, nullptr, stream_info);
+  // Access log (report).
+  filter.log(&downstream_headers, &upstream_headers, &upstream_trailers,
+             stream_info);
 }
 
 DEFINE_PROTO_FUZZER(
@@ -80,128 +103,115 @@ DEFINE_PROTO_FUZZER(
     TestUtility::validate(input);
 
     // Validate nested protos with stricter requirements for the fuzz test.
-    // We only need 1 requirement in the config, others will just add noise.
-    if (input.config().requirements_size() != 1) {
-      throw ProtoValidationException("requirements", input);
+    // We need at least 1 requirement in the config to match a selector.
+    if (input.config().requirements_size() < 1) {
+      throw ProtoValidationException("Need at least 1 requirement", input);
     }
-    // We only expect 1 buffer in the body to simplify setup.
-    if (input.downstream_request().data().size() != 1) {
-      throw ProtoValidationException("downstream data", input);
-    }
-    if (input.upstream_response().data().size() != 1) {
-      throw ProtoValidationException("upstream data", input);
-    }
-    for (auto& sidestream_response : input.sidestream_response()) {
-      if (sidestream_response.data().size() != 1) {
-        throw ProtoValidationException("sidestream data", input);
-      }
-    }
-    // There should be at least 1 sidestream response, otherwise no point.
-    if (input.sidestream_response().size() < 1) {
-      throw ProtoValidationException("num sidestream", input);
-    }
-
-    // Setup mocks.
-    NiceMock<MockFactoryContext> context;
-    NiceMock<Http::MockAsyncClientRequest> response(
-        &context.cluster_manager_.async_client_);
-    NiceMock<Envoy::Http::MockStreamDecoderFilterCallbacks>
-        mock_decoder_callbacks;
-    NiceMock<Envoy::Http::MockStreamEncoderFilterCallbacks>
-        mock_encoder_callbacks;
-
-    // Return a fake span.
-    EXPECT_CALL(mock_decoder_callbacks, activeSpan())
-        .WillRepeatedly(ReturnRef(Envoy::Tracing::NullSpan::instance()));
-
-    // Callback for token subscriber to start.
-    Envoy::Event::TimerCb onReadyCallback;
-    EXPECT_CALL(context.dispatcher_, createTimer_(_))
-        .WillRepeatedly(
-            Invoke([&onReadyCallback](const Envoy::Event::TimerCb& cb) {
-              ENVOY_LOG_MISC(trace, "Mocking dispatcher createTimer");
-              onReadyCallback = cb;
-              return new NiceMock<Event::MockTimer>();
-            }));
-
-    // Mock the http async client.
-    int resp_num = 0;
-    EXPECT_CALL(context.cluster_manager_.async_client_, send_(_, _, _))
-        .WillRepeatedly(Invoke([&response, &input, &resp_num](
-                                   const Envoy::Http::RequestMessagePtr&,
-                                   Envoy::Http::AsyncClient::Callbacks&
-                                       callback,
-                                   const Envoy::Http::AsyncClient::
-                                       RequestOptions&) {
-          // FIXME(nareddyt): For now, just increment the counter for response
-          // numbers.
-          auto& response_data = input.sidestream_response().Get(
-              resp_num++ % input.sidestream_response().size());
-
-          // Create the response message.
-          auto headers =
-              Envoy::Fuzz::fromHeaders<Envoy::Http::TestResponseHeaderMapImpl>(
-                  response_data.headers());
-          auto headers_ptr =
-              std::make_unique<Envoy::Http::TestResponseHeaderMapImpl>(headers);
-          auto trailers =
-              Envoy::Fuzz::fromHeaders<Envoy::Http::TestResponseTrailerMapImpl>(
-                  response_data.trailers());
-          auto trailers_ptr =
-              std::make_unique<Envoy::Http::TestResponseTrailerMapImpl>(
-                  trailers);
-
-          auto msg = std::make_unique<Envoy::Http::ResponseMessageImpl>(
-              std::move(headers_ptr));
-          msg->trailers(std::move(trailers_ptr));
-          msg->body() =
-              std::make_unique<Buffer::OwnedImpl>(response_data.data().Get(0));
-
-          // Callback.
-          callback.onSuccess(std::move(msg));
-          return &response;
-        }));
-
-    // Fuzz the stream info.
-    TestStreamInfo stream_info =
-        Envoy::Fuzz::fromStreamInfo(input.stream_info());
-    EXPECT_CALL(mock_decoder_callbacks, streamInfo())
-        .WillRepeatedly(ReturnRef(stream_info));
-    EXPECT_CALL(mock_encoder_callbacks, streamInfo())
-        .WillRepeatedly(ReturnRef(stream_info));
-
-    try {
-      // Create filter config.
-      ServiceControlFilterConfig filter_config(input.config(),
-                                               "fuzz-test-stats", context);
-
-      // Set the operation name to match an endpoint that requires API keys
-      // and has configured metric costs.
-      // This ensures both CHECK and QUOTA are called.
-      Utils::setStringFilterState(
-          *stream_info.filter_state_, Utils::kOperation,
-          input.config().requirements(0).operation_name());
-
-      // Create filter.
-      ServiceControlFilter filter(filter_config.stats(),
-                                  filter_config.handler_factory());
-      filter.setDecoderFilterCallbacks(mock_decoder_callbacks);
-      filter.setEncoderFilterCallbacks(mock_encoder_callbacks);
-
-      if (onReadyCallback != nullptr) {
-        // Filter config is valid enough to start the token subscriber.
-        onReadyCallback();
-      }
-
-      // Run data against the filter.
-      doTest(filter, stream_info, input);
-
-    } catch (const EnvoyException& e) {
-      ENVOY_LOG_MISC(debug, "Controlled envoy exception: {}", e.what());
-    }
-
   } catch (const ProtoValidationException& e) {
     ENVOY_LOG_MISC(debug, "Controlled proto validation failure: {}", e.what());
+    return;
+  }
+
+  // Setup mocks.
+  NiceMock<MockFactoryContext> context;
+  NiceMock<Http::MockAsyncClientRequest> request(
+      &context.cluster_manager_.async_client_);
+  NiceMock<Envoy::Http::MockStreamDecoderFilterCallbacks>
+      mock_decoder_callbacks;
+  NiceMock<Envoy::Http::MockStreamEncoderFilterCallbacks>
+      mock_encoder_callbacks;
+
+  // Return a fake span.
+  EXPECT_CALL(mock_decoder_callbacks, activeSpan())
+      .WillRepeatedly(ReturnRef(Envoy::Tracing::NullSpan::instance()));
+
+  // Callback for token subscriber to start.
+  Envoy::Event::TimerCb onReadyCallback;
+  EXPECT_CALL(context.dispatcher_, createTimer_(_))
+      .WillRepeatedly(
+          Invoke([&onReadyCallback](const Envoy::Event::TimerCb& cb) {
+            ENVOY_LOG_MISC(trace, "Mocking dispatcher createTimer");
+            onReadyCallback = cb;
+            return new NiceMock<Event::MockTimer>();
+          }));
+
+  // Mock the http async client.
+  int resp_num = 0;
+  EXPECT_CALL(context.cluster_manager_.async_client_, send_(_, _, _))
+      .WillRepeatedly(Invoke([&request, &input, &resp_num](
+                                 const Envoy::Http::RequestMessagePtr&,
+                                 Envoy::Http::AsyncClient::Callbacks& callback,
+                                 const Envoy::Http::AsyncClient::
+                                     RequestOptions&) {
+        // FIXME(nareddyt): For now, just increment the counter for
+        // response numbers.
+        auto& response_data = input.sidestream_response().Get(
+            resp_num++ % input.sidestream_response().size());
+
+        // Create the response message.
+        auto headers =
+            Envoy::Fuzz::fromHeaders<Envoy::Http::TestResponseHeaderMapImpl>(
+                response_data.headers());
+        auto headers_ptr =
+            std::make_unique<Envoy::Http::TestResponseHeaderMapImpl>(headers);
+        auto trailers =
+            Envoy::Fuzz::fromHeaders<Envoy::Http::TestResponseTrailerMapImpl>(
+                response_data.trailers());
+        auto trailers_ptr =
+            std::make_unique<Envoy::Http::TestResponseTrailerMapImpl>(trailers);
+
+        auto msg = std::make_unique<Envoy::Http::ResponseMessageImpl>(
+            std::move(headers_ptr));
+        msg->trailers(std::move(trailers_ptr));
+        if (response_data.data_size() > 0) {
+          // FIXME(nareddyt): For now, just grab 1 data item from the
+          // proto.
+          msg->body() =
+              std::make_unique<Buffer::OwnedImpl>(response_data.data().Get(0));
+        } else {
+          msg->body() = std::make_unique<Buffer::OwnedImpl>();
+        }
+
+        // Callback.
+        callback.onSuccess(std::move(msg));
+        return &request;
+      }));
+
+  // Fuzz the stream info.
+  TestStreamInfo stream_info = Envoy::Fuzz::fromStreamInfo(input.stream_info());
+  EXPECT_CALL(mock_decoder_callbacks, streamInfo())
+      .WillRepeatedly(ReturnRef(stream_info));
+  EXPECT_CALL(mock_encoder_callbacks, streamInfo())
+      .WillRepeatedly(ReturnRef(stream_info));
+
+  try {
+    // Create filter config.
+    ServiceControlFilterConfig filter_config(input.config(), "fuzz-test-stats",
+                                             context);
+
+    // Set the operation name to match an endpoint that requires API keys
+    // and has configured metric costs.
+    // This ensures both CHECK and QUOTA are called.
+    Utils::setStringFilterState(
+        *stream_info.filter_state_, Utils::kOperation,
+        input.config().requirements(0).operation_name());
+
+    // Create filter.
+    ServiceControlFilter filter(filter_config.stats(),
+                                filter_config.handler_factory());
+    filter.setDecoderFilterCallbacks(mock_decoder_callbacks);
+    filter.setEncoderFilterCallbacks(mock_encoder_callbacks);
+
+    if (onReadyCallback != nullptr) {
+      // Filter config is valid enough to start the token subscriber.
+      onReadyCallback();
+    }
+
+    // Run data against the filter.
+    doTest(filter, stream_info, input);
+
+  } catch (const EnvoyException& e) {
+    ENVOY_LOG_MISC(debug, "Controlled envoy exception: {}", e.what());
   }
 }
 
