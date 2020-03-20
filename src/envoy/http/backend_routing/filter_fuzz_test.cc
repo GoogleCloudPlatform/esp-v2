d60c178b560652dea4ac92ece650fd394588127f
Teju Nareddy <nareddyt@google.com>
2020-03-20 09:52:07 -0700

    Ignore invalid http header characters in backend routing filter fuzz test
    Path matcher ensures the `binding_query_params` has valid header values. Prevent the fuzzer from generating invalid characters.
    
    Added reproducer test case.
    
    Ref: https://bugs.chromium.org/p/oss-fuzz/issues/detail?id=21229
    Signed-off-by: Teju Nareddy <nareddyt@google.com>

--- src/envoy/http/backend_routing/filter_fuzz_test.cc
+++ src/envoy/http/backend_routing/filter_fuzz_test.cc
@@ -22,46 +22,43 @@ DEFINE_PROTO_FUZZER(
   try {
     TestUtility::validate(input);
 
-    // This fuzz test only requires a single backend routing rule.
-    // All other rules are ignored. So improve performance by only allowing
-    // configs with one rule through.
-    if (input.config().rules_size() != 1) {
-      throw ProtoValidationException("Only 1 backend rule is allowed", input);
+    if (input.config().rules_size() < 1) {
+      throw ProtoValidationException("At least 1 backend rule needed", input);
     }
-
-    // Setup mocks.
-    NiceMock<Envoy::Http::MockStreamDecoderFilterCallbacks>
-        mock_decoder_callbacks;
-    NiceMock<Envoy::Server::Configuration::MockFactoryContext>
-        mock_factory_context;
-
-    // Set the operation name using the first backend routing rule.
-    Utils::setStringFilterState(
-        *mock_decoder_callbacks.stream_info_.filter_state_, Utils::kOperation,
-        input.config().rules(0).operation());
-
-    // Set the variable binding query params.
-    Utils::setStringFilterState(
-        *mock_decoder_callbacks.stream_info_.filter_state_, Utils::kQueryParams,
-        input.binding_query_params());
-
-    // Create the filter.
-    FilterConfigSharedPtr config = std::make_shared<FilterConfig>(
-        input.config(), "fuzz-test-stats", mock_factory_context);
-    Filter filter(config);
-    filter.setDecoderFilterCallbacks(mock_decoder_callbacks);
-
-    // Generate the user request.
-    auto headers =
-        Envoy::Fuzz::fromHeaders<Envoy::Http::TestRequestHeaderMapImpl>(
-            input.user_request().headers());
-
-    // Functions under test.
-    filter.decodeHeaders(headers, false);
-
   } catch (const ProtoValidationException& e) {
     ENVOY_LOG_MISC(debug, "Controlled proto validation failure: {}", e.what());
+    return;
   }
+
+  // Setup mocks.
+  NiceMock<Envoy::Http::MockStreamDecoderFilterCallbacks>
+      mock_decoder_callbacks;
+  NiceMock<Envoy::Server::Configuration::MockFactoryContext>
+      mock_factory_context;
+
+  // Set the operation name using the first backend routing rule.
+  Utils::setStringFilterState(
+      *mock_decoder_callbacks.stream_info_.filter_state_, Utils::kOperation,
+      input.config().rules(0).operation());
+
+  // Set the variable binding query params.
+  Utils::setStringFilterState(
+      *mock_decoder_callbacks.stream_info_.filter_state_, Utils::kQueryParams,
+      input.binding_query_params());
+
+  // Create the filter.
+  FilterConfigSharedPtr config = std::make_shared<FilterConfig>(
+      input.config(), "fuzz-test-stats", mock_factory_context);
+  Filter filter(config);
+  filter.setDecoderFilterCallbacks(mock_decoder_callbacks);
+
+  // Generate the user request.
+  auto headers =
+      Envoy::Fuzz::fromHeaders<Envoy::Http::TestRequestHeaderMapImpl>(
+          input.user_request().headers());
+
+  // Functions under test.
+  filter.decodeHeaders(headers, false);
 }
 
 }  // namespace BackendRouting
