8b0d1607e7b178df53868b9d247032aaebe55e28
Xuyang Tao <taoxuy@google.com>
2020-03-23 19:56:04 -0700

    reuse unitest server

--- src/go/serviceconfig/rollout_id_change_fetcher_test.go
+++ src/go/serviceconfig/rollout_id_change_fetcher_test.go
@@ -17,7 +17,6 @@ package serviceconfig
 import (
 	"fmt"
 	"net/http"
-	"net/http/httptest"
 	"testing"
 	"time"
 
@@ -27,28 +26,19 @@ import (
 	scpb "google.golang.org/genproto/googleapis/api/servicecontrol/v1"
 )
 
-func initServiceControlForTestNewRolloutId(t *testing.T, serviceRolloutId *string) *httptest.Server {
-	return httptest.NewServer(http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
-		resp, err := genFakeReport(*serviceRolloutId)
-		if err != nil {
-			t.Fatalf("fail to generate servicecontrol report response: %v", err)
-		}
-		_, _ = w.Write(resp)
-	}))
-}
-
-func genFakeReport(serviceRolloutId string) ([]byte, error) {
+func genFakeReport(serviceRolloutId string) string {
 	reportResp := new(scpb.ReportResponse)
 	reportResp.ServiceRolloutId = serviceRolloutId
-	return proto.Marshal(reportResp)
+	reportRespBytes, _ := proto.Marshal(reportResp)
+	return string(reportRespBytes)
 }
 
 func TestFetchLatestRolloutId(t *testing.T) {
 	serviceRolloutId := "service-config-id"
-	serviceControlServer := initServiceControlForTestNewRolloutId(t, &serviceRolloutId)
+	serviceControlServer := util.InitMockServer(genFakeReport(serviceRolloutId))
 	accessToken := func() (string, time.Duration, error) { return "token", time.Duration(60), nil }
 
-	cif := NewRolloutIdChangeDetector(&http.Client{}, serviceControlServer.URL, "service-name", accessToken)
+	cif := NewRolloutIdChangeDetector(&http.Client{}, serviceControlServer.GetURL(), "service-name", accessToken)
 
 	// Test success of fetching the latest rolloutId.
 	rolloutId, _ := cif.fetchLatestRolloutId()
@@ -71,9 +61,9 @@ func TestFetchLatestRolloutId(t *testing.T) {
 
 func TestRolloutIdChangeFetcherSetDetectRolloutIdChangeTimer(t *testing.T) {
 	serviceRolloutId := "service-config-id"
-	serviceControlServer := initServiceControlForTestNewRolloutId(t, &serviceRolloutId)
+	serviceControlServer := util.InitMockServer(genFakeReport(serviceRolloutId))
 	accessToken := func() (string, time.Duration, error) { return "token", time.Duration(60), nil }
-	cif := NewRolloutIdChangeDetector(&http.Client{}, serviceControlServer.URL, "service-name", accessToken)
+	cif := NewRolloutIdChangeDetector(&http.Client{}, serviceControlServer.GetURL(), "service-name", accessToken)
 
 	cnt := 0
 	wantCnt := 3
@@ -84,6 +74,7 @@ func TestRolloutIdChangeFetcherSetDetectRolloutIdChangeTimer(t *testing.T) {
 		// It will be updated only three times.
 		if cnt < wantCnt {
 			serviceRolloutId = fmt.Sprintf("test-rollout-id-%v", cnt+1)
+			serviceControlServer.SetResp(genFakeReport(serviceRolloutId))
 		}
 	})
 
