8db1233a0a12550afbff8e069e7ea84259a4ed27
Xuyang Tao <taoxuy@google.com>
2020-03-30 12:56:08 -0700

    b/149484856: add additional jwtLocation e2e tests

--- tests/e2e/client/apiproxy_bookstore_test.py
+++ tests/e2e/client/apiproxy_bookstore_test.py
@@ -165,6 +165,21 @@ class ApiProxyBookstoreTest(ApiProxyClientTest):
             '/shelves?{}'.format('jwt_query_name=' + FLAGS.auth_token))
         self.assertEqual(response.status_code, 200)
 
+        # `X-Goog-Iap-Jwt-Assertion` is a default jwt location but it is not set
+        # in the customized x-google-jwt-locations.
+        response = self._call_http(
+            '/shelves', userHeaders={"X-Goog-Iap-Jwt-Assertion": FLAGS.auth_token})
+        self.assertEqual(response.status_code, 401)
+
+        # `Authorization`is a default jwt location and is also set the in
+        # customized x-google-jwt-locations.
+        response = self._call_http(
+            '/shelves', userHeaders={
+            "X-Goog-Iap-Jwt-Assertion": "Invalid-Token",
+            "Authorization": "Bearer {}".format(FLAGS.auth_token)
+        })
+        self.assertEqual(response.status_code, 200)
+
 
     def run_all_tests(self):
         shelf1 = {
