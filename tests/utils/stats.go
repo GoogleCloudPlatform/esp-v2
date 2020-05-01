1fcd2262759d360ed5e642792261747c3f1a1f00
Xuyang Tao <taoxuy@google.com>
2020-05-01 15:44:47 -0700

    add license

--- tests/utils/stats.go
+++ tests/utils/stats.go
@@ -1,4 +1,17 @@
-// Stats is the struct to decode envoy admin json raw data.
+// Copyright 2019 Google LLC
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//      http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
 package utils
 
 import (
@@ -7,6 +20,7 @@ import (
 	"sort"
 )
 
+// Stats is the struct to decode envoy admin json raw data.
 type Stats struct {
 	Stat []StatsData `json:"stats"`
 }
