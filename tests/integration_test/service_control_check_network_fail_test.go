59c54937e547fd7a561178c3bfdfa99cc30bd80f
Teju Nareddy <nareddyt@google.com>
2020-05-08 12:16:59 -0700

    Add integration test
    Signed-off-by: Teju Nareddy <nareddyt@google.com>

--- tests/integration_test/service_control_check_network_fail_test.go
+++ tests/integration_test/service_control_check_network_fail_test.go
@@ -27,6 +27,7 @@ import (
 
 	bsclient "github.com/GoogleCloudPlatform/esp-v2/tests/endpoints/bookstore_grpc/client"
 	comp "github.com/GoogleCloudPlatform/esp-v2/tests/env/components"
+	scpb "google.golang.org/genproto/googleapis/api/servicecontrol/v1"
 )
 
 func TestServiceControlCheckNetworkFail(t *testing.T) {
@@ -151,7 +152,7 @@ func (h *localServiceHandler) ServeHTTP(w http.ResponseWriter, r *http.Request) 
 	w.Write([]byte(""))
 }
 
-func TestServiceControlNetworkFailFlag(t *testing.T) {
+func TestServiceControlNetworkFailFlagForTimeout(t *testing.T) {
 	t.Parallel()
 
 	serviceName := "bookstore-service"
@@ -162,7 +163,6 @@ func TestServiceControlNetworkFailFlag(t *testing.T) {
 	tests := []struct {
 		desc            string
 		networkFailOpen bool
-		port            uint16
 		clientProtocol  string
 		httpMethod      string
 		method          string
@@ -174,7 +174,6 @@ func TestServiceControlNetworkFailFlag(t *testing.T) {
 		{
 			desc:            "Successful, since service_control_network_fail_open is set as true, the timeout of service control check response will be ignored.",
 			networkFailOpen: true,
-			port:            comp.TestServiceControlNetworkFailFlagOpen,
 			clientProtocol:  "http",
 			httpMethod:      "GET",
 			method:          "/v1/shelves?key=api-key",
@@ -182,9 +181,8 @@ func TestServiceControlNetworkFailFlag(t *testing.T) {
 			wantResp:        `{"shelves":[{"id":"100","theme":"Kids"},{"id":"200","theme":"Classic"}]}`,
 		},
 		{
-			desc:            "Failed, since service_control_network_fail_open is set as true default, the timeout of service control check response won't be ignored.",
+			desc:            "Failed, since service_control_network_fail_open is set as false, the timeout of service control check response won't be ignored.",
 			networkFailOpen: false,
-			port:            comp.TestServiceControlNetworkFailFlagClosed,
 			clientProtocol:  "http",
 			httpMethod:      "GET",
 			method:          "/v1/shelves?key=api-key",
@@ -195,7 +193,7 @@ func TestServiceControlNetworkFailFlag(t *testing.T) {
 
 	for _, tc := range tests {
 		func() {
-			s := env.NewTestEnv(tc.port, platform.GrpcBookstoreSidecar)
+			s := env.NewTestEnv(comp.TestServiceControlNetworkFailFlagForTimeout, platform.GrpcBookstoreSidecar)
 			s.ServiceControlServer.OverrideCheckHandler(&localServiceHandler{
 				m: s.ServiceControlServer,
 			})
@@ -209,6 +207,101 @@ func TestServiceControlNetworkFailFlag(t *testing.T) {
 			}
 
 			s.ServiceControlServer.ResetRequestCount()
+			addr := fmt.Sprintf("localhost:%v", s.Ports().ListenerPort)
+			resp, err := bsclient.MakeCall(tc.clientProtocol, addr, tc.httpMethod, tc.method, tc.token, nil)
+
+			if tc.wantError != "" && (err == nil || !strings.Contains(err.Error(), tc.wantError)) {
+				t.Errorf("Test (%s): failed, expected err: %v, got: %v", tc.desc, tc.wantError, err)
+			} else if !strings.Contains(resp, tc.wantResp) {
+				t.Errorf("Test (%s): failed, expected: %s, got: %s", tc.desc, tc.wantResp, resp)
+			}
+		}()
+	}
+}
+
+func TestServiceControlNetworkFailFlagForUnavailableCheckResponse(t *testing.T) {
+	t.Parallel()
+
+	serviceName := "bookstore-service"
+	configID := "test-config-id"
+	args := []string{"--service=" + serviceName, "--service_config_id=" + configID,
+		"--rollout_strategy=fixed"}
+
+	tests := []struct {
+		desc            string
+		networkFailOpen bool
+		checkResponse   scpb.CheckResponse
+		clientProtocol  string
+		httpMethod      string
+		method          string
+		token           string
+		checkFailStatus int
+		wantResp        string
+		wantError       string
+	}{
+		{
+			desc:            "Successful, since service_control_network_fail_open is set as true, the unavailable check error will be ignored.",
+			networkFailOpen: true,
+			checkResponse: scpb.CheckResponse{
+				CheckErrors: []*scpb.CheckError{
+					{
+						Code: scpb.CheckError_NAMESPACE_LOOKUP_UNAVAILABLE,
+					},
+				},
+			},
+			clientProtocol: "http",
+			httpMethod:     "GET",
+			method:         "/v1/shelves?key=api-key",
+			token:          testdata.FakeCloudTokenMultiAudiences,
+			wantResp:       `{"shelves":[{"id":"100","theme":"Kids"},{"id":"200","theme":"Classic"}]}`,
+		},
+		{
+			desc:            "Failed, since service_control_network_fail_open is set as false, the unavailable check error won't be ignored.",
+			networkFailOpen: false,
+			checkResponse: scpb.CheckResponse{
+				CheckErrors: []*scpb.CheckError{
+					{
+						Code: scpb.CheckError_NAMESPACE_LOOKUP_UNAVAILABLE,
+					},
+				},
+			},
+			clientProtocol: "http",
+			httpMethod:     "GET",
+			method:         "/v1/shelves?key=api-key",
+			token:          testdata.FakeCloudTokenMultiAudiences,
+			wantError:      "503 Service Unavailable, UNAVAILABLE:One or more Google Service Control backends are unavailable.",
+		},
+		{
+			desc:            "Failed, even though service_control_network_fail_open is set as true, non-5xx check error won't be ignored.",
+			networkFailOpen: true,
+			checkResponse: scpb.CheckResponse{
+				CheckErrors: []*scpb.CheckError{
+					{
+						Code: scpb.CheckError_PROJECT_INVALID,
+					},
+				},
+			},
+			clientProtocol: "http",
+			httpMethod:     "GET",
+			method:         "/v1/shelves?key=api-key",
+			token:          testdata.FakeCloudTokenMultiAudiences,
+			wantError:      "400 Bad Request, INVALID_ARGUMENT:Client project not valid. Please pass a valid project.",
+		},
+	}
+
+	for _, tc := range tests {
+		func() {
+			s := env.NewTestEnv(comp.TestServiceControlNetworkFailFlagForUnavailableCheckResponse, platform.GrpcBookstoreSidecar)
+			s.ServiceControlServer.SetCheckResponse(&tc.checkResponse)
+			if tc.networkFailOpen {
+				s.EnableScNetworkFailOpen()
+			}
+
+			defer s.TearDown()
+			if err := s.Setup(args); err != nil {
+				t.Fatalf("fail to setup test env, %v", err)
+			}
+
 			addr := fmt.Sprintf("localhost:%v", s.Ports().ListenerPort)
 			resp, err := bsclient.MakeCall(tc.clientProtocol, addr, tc.httpMethod, tc.method, tc.token, nil)
 
