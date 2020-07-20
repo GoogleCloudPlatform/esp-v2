19c528291a4d4f119b0c71ffa9fcd6d28cd5de61
Teju Nareddy <nareddyt@google.com>
2020-07-20 13:41:04 -0700

    Review comments
    Signed-off-by: Teju Nareddy <nareddyt@google.com>

--- prow/e2e-gcloud-build-image.sh
+++ prow/e2e-gcloud-build-image.sh
@@ -2,6 +2,20 @@
 
 # E2E test for gcloud_build_image script.
 
+# Copyright 2020 Google LLC
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#    https://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
 # Fail on any error.
 set -eo pipefail
 
@@ -13,8 +27,8 @@ gcloud config set project "${PROJECT_NAME}"
 export CLOUDSDK_CORE_DISABLE_PROMPTS=1
 
 # Use a service that is not used for any other tests.
-SERVICE_NAME="hellogrpc.endpoints.cloudesf-testing.cloud.goog"
-CONFIG_ID="2018-11-11r0"
+SERVICE_NAME="gcloud-build-image-e2e-test.endpoints.cloudesf-testing.cloud.goog"
+CONFIG_ID="2020-07-20r1"
 
 function error_exit() {
   # ${BASH_SOURCE[1]} is the file name of the caller.
