1d94fa5feb917b58c3bb5d03be96b82330f4398c
Xuyang Tao <taoxuy@google.com>
2020-08-21 13:01:48 -0700

    tmp

--- scripts/robot-release.sh
+++ scripts/robot-release.sh
@@ -56,6 +56,7 @@ checkImageExistence $(get_envoy_image_name) $(get_tag_name)  \
   && { echo "Images $(get_envoy_image_name_with_sha), $(get_gcsrunner_image_name_with_sha), and $(get_proxy_image_name_with_sha) already exists; Skip.";
 exit 0; }
 
+gcloud config list account --format "value(core.account)"
 upload_envoy_binary
 
 echo "Images $(get_envoy_image_name_with_sha), $(get_gcsrunner_image_name_with_sha), and $(get_proxy_image_name_with_sha) don't exist; Start to build."
