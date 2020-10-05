96c61ffd01f1c6eb0a90aa96aa8d1e0143f147dd
Xuyang Tao <taoxuy@google.com>
2020-10-05 11:29:59 -0700

    Fix api regression test (#361)

--- scripts/all-utilities.sh
+++ scripts/all-utilities.sh
@@ -413,15 +413,3 @@ function sleep_wrapper() {
   sleep ${length}
   echo "$(date): End sleep"
 }
-
-function envoy_binary_gcs_path() {
-  echo -n "gs://apiproxy-testing-envoy-binaries/$(get_sha)"
-}
-
-function upload_envoy_binary() {
-  gsutil -m cp ${ROOT}/bin/envoy $(envoy_binary_gcs_path)
-}
-
-function download_envoy_binary() {
-  gsutil -m cp $(envoy_binary_gcs_path)  ${ROOT}/bin/envoy
-}
