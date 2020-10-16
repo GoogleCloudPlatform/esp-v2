b57232899f887a6791670160031fb044efce15c1
Wayne Zhang <qiwzhang@google.com>
2020-10-16 09:28:17 -0700

    add build
    Signed-off-by: Wayne Zhang <qiwzhang@google.com>

--- src/envoy/utils/rc_detail_utils.h
+++ src/envoy/utils/rc_detail_utils.h
@@ -26,6 +26,7 @@ const char kRcDetailFilterServiceControl[] = "service_control";
 const char kRcDetailFilterPathMatcher[] = "path_matcher";
 const char kRcDetailFilterBackendAuth[] = "backend_auth";
 const char kRcDetailFilterBackendRouting[] = "backend_routing";
+const char kRcDetailFilterPathRewrite[] = "path_rewrite";
 
 // The error types
 //
