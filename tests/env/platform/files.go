db1a6a00fec940c84ca98110c10dd8b7c38819e5
Wayne Zhang <qiwzhang@google.com>
2020-11-04 09:42:35 -0800

    Add path_matcher example back as route_match
    Signed-off-by: Wayne Zhang <qiwzhang@google.com>

--- tests/env/platform/files.go
+++ tests/env/platform/files.go
@@ -55,6 +55,8 @@ const (
 	ScEnvoyConfig
 	DrServiceConfig
 	DrEnvoyConfig
+	RmServiceConfig
+	RmEnvoyConfig
 	SbServiceConfig
 	SbEnvoyConfig
 	GrpcEchoServiceConfig
@@ -101,6 +103,8 @@ var fileMap = map[RuntimeFile]string{
 	ScEnvoyConfig:         "../../../../examples/service_control/envoy_config.json",
 	DrServiceConfig:       "../../../../examples/dynamic_routing/service_config_generated.json",
 	DrEnvoyConfig:         "../../../../examples/dynamic_routing/envoy_config.json",
+	RmServiceConfig:       "../../../../examples/testdata/route_match/service_config_generated.json",
+	RmEnvoyConfig:         "../../../../examples/testdata/route_match/envoy_config.json",
 	SbServiceConfig:       "../../../../examples/testdata/sidecar_backend/service_config_generated.json",
 	SbEnvoyConfig:         "../../../../examples/testdata/sidecar_backend/envoy_config.json",
 	GrpcEchoServiceConfig: "../../../../examples/grpc_dynamic_routing/service_config_generated.json",
