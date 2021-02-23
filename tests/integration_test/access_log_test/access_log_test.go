4f8741bd9898e829021e5cc41e85d97a5020f7d2
Teju Nareddy <nareddyt@google.com>
2021-02-23 11:50:58 -0800

    Fix package name
    Signed-off-by: Teju Nareddy <nareddyt@google.com>

--- tests/integration_test/access_log_test/access_log_test.go
+++ tests/integration_test/access_log_test/access_log_test.go
@@ -12,7 +12,7 @@
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
-package access_log_test_test
+package access_log_test
 
 import (
 	"fmt"
