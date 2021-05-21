e3540416a5ce562271f427f6b1e84669adb020f5
Teju Nareddy <nareddyt@google.com>
2021-05-21 10:37:02 -0700

    b/188808823: Simplify CORS regex and fix brittle tests
    Signed-off-by: Teju Nareddy <nareddyt@google.com>

--- src/go/configgenerator/route_generator_test.go
+++ src/go/configgenerator/route_generator_test.go
@@ -3236,7 +3236,7 @@ func TestMakeRouteConfigForCors(t *testing.T) {
 		{
 			desc:        "Oversize cors origin regex",
 			params:      []string{"cors_with_regex", "", getOverSizeRegexForTest(), "", "Origin,Content-Type,Accept", "", "2m"},
-			wantedError: `invalid cors origin regex: regex program size(1001) is larger than the max expected(1000)`,
+			wantedError: `invalid cors origin regex: regex program size`,
 		},
 		{
 			desc:   "Correct configured basic Cors, with allow methods",
@@ -3487,7 +3487,7 @@ func getOverSizeRegexForTest() string {
 	overSizeRegex := ""
 	for i := 0; i < 333; i += 1 {
 		// Use "/**" as it is a replacement token for wildcard uri template.
-		overSizeRegex += "/**"
+		overSizeRegex += "/**/a"
 	}
 	return overSizeRegex
 }
