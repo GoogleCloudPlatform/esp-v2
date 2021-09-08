c9258b2d18bc6977f6d4f72fdf92c12224ad28b5
Sophie Yang <sophyang@google.com>
2021-09-08 15:36:16 +0000

    Allow command line arg to override default stderrthreshold

--- src/go/gcsrunner/main/runner.go
+++ src/go/gcsrunner/main/runner.go
@@ -60,14 +60,17 @@ var (
 )
 
 func main() {
-	flag.Parse()
-
-	gcsrunnerLogLevel := os.Getenv("GCSRUNNER_LOG_LEVEL")
-	if gcsrunnerLogLevel == "" {
-		gcsrunnerLogLevel = gcsrunnerDefaultLogLevel
+	err := setGcsrunnerLogLevel(gcsrunnerDefaultLogLevel); if err != nil {
+		glog.Errorf("Failed to override flag --stderrthreshold to default log level %s: %v", gcsrunnerDefaultLogLevel, err)
 	}
-	if err := flag.Set("stderrthreshold", gcsrunnerLogLevel); err != nil {
-		glog.Errorf("Failed to override flag --stderrthreshold to %s: %v", gcsrunnerLogLevel, err)
+
+	flag.Parse()
+	
+	gcsrunnerLogLevel := os.Getenv("GCSRUNNER_LOG_LEVEL")
+	if gcsrunnerLogLevel != "" {
+		err := setGcsrunnerLogLevel(gcsrunnerLogLevel); if err != nil {
+			glog.Errorf("Failed to override flag --stderrthreshold to log level %s: %v", gcsrunnerLogLevel, err)
+		}
 	}
 
 	bucketName := os.Getenv("BUCKET")
@@ -131,6 +134,10 @@ func main() {
 	}); err != nil {
 		glog.Fatalf("Envoy erred: %v", err)
 	}
+}
+
+func setGcsrunnerLogLevel(logLevel string) error {
+	return flag.Set("stderrthreshold", logLevel)
 }
 
 func envNum(v string, defaultVal uint32) (uint32, error) {
