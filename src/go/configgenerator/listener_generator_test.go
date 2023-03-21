45095dc4add0bb9c48224091e8df8d7e1f4fb4a0
Shuo Yang <yangshuo@google.com>
2023-03-21 15:21:13 -0700

    Support http backend override in http backend rule. This is noop for espv2. (#794)
    cl/509959486 in google3.

--- src/go/configgenerator/listener_generator_test.go
+++ src/go/configgenerator/listener_generator_test.go
@@ -19,19 +19,30 @@ import (
 	"github.com/GoogleCloudPlatform/esp-v2/src/go/options"
 	"github.com/GoogleCloudPlatform/esp-v2/src/go/util"
 	"github.com/golang/protobuf/jsonpb"
+	"google.golang.org/protobuf/types/known/anypb"
 
 	corepb "github.com/envoyproxy/go-control-plane/envoy/config/core/v3"
 	routepb "github.com/envoyproxy/go-control-plane/envoy/config/route/v3"
 	hcmpb "github.com/envoyproxy/go-control-plane/envoy/extensions/filters/network/http_connection_manager/v3"
 	structpb "github.com/golang/protobuf/ptypes/struct"
+	annotationspb "google.golang.org/genproto/googleapis/api/annotations"
 	confpb "google.golang.org/genproto/googleapis/api/serviceconfig"
+	smpb "google.golang.org/genproto/googleapis/api/servicemanagement/v1"
 	apipb "google.golang.org/genproto/protobuf/api"
 )
 
 func TestMakeListeners(t *testing.T) {
+	configFile := &smpb.ConfigFile{
+		FileType: smpb.ConfigFile_FILE_DESCRIPTOR_SET_PROTO,
+	}
+	data, err := anypb.New(configFile)
+	if err != nil {
+		t.Fatal(err)
+	}
 	testdata := []struct {
 		desc              string
 		sslServerCertPath string
+		testGrpc          bool
 		fakeServiceConfig *confpb.Service
 		wantListeners     []string
 	}{
@@ -156,6 +167,221 @@ func TestMakeListeners(t *testing.T) {
           }
         }
       }
+    }
+  ],
+  "name": "ingress_listener",
+  "perConnectionBufferLimitBytes": 1024
+}`,
+			},
+		},
+		{
+			desc:     "Success, http backend in the backend rule",
+			testGrpc: true,
+			fakeServiceConfig: &confpb.Service{
+				Name: testProjectName,
+				Apis: []*apipb.Api{
+					{
+						Name: "endpoints.examples.bookstore.Bookstore",
+						Methods: []*apipb.Method{
+							{
+								Name: "CreateShelf",
+							},
+						},
+					},
+				},
+				Http: &annotationspb.Http{
+					Rules: []*annotationspb.HttpRule{
+						{
+							Selector: "endpoints.examples.bookstore.Bookstore.CreateShelf",
+							Pattern: &annotationspb.HttpRule_Get{
+								Get: "/CreateShelf",
+							},
+						},
+					},
+				},
+				Backend: &confpb.Backend{
+					Rules: []*confpb.BackendRule{
+						{
+							Selector: "endpoints.examples.bookstore.Bookstore.CreateShelf",
+							OverridesByRequestProtocol: map[string]*confpb.BackendRule{
+								"http": &confpb.BackendRule{
+									Address: "http://http.backend.test:8080",
+								},
+							},
+						},
+					},
+				},
+				SourceInfo: &confpb.SourceInfo{
+					SourceFiles: []*anypb.Any{
+						data,
+					},
+				},
+			},
+			wantListeners: []string{`
+{
+  "address": {
+    "socketAddress": {
+      "address": "0.0.0.0",
+      "portValue": 8080
+    }
+  },
+  "filterChains": [
+    {
+      "filters": [
+        {
+          "name": "envoy.filters.network.http_connection_manager",
+          "typedConfig": {
+            "@type": "type.googleapis.com/envoy.extensions.filters.network.http_connection_manager.v3.HttpConnectionManager",
+            "commonHttpProtocolOptions": {},
+            "httpFilters": [
+              {
+                "name": "com.google.espv2.filters.http.grpc_metadata_scrubber",
+                "typedConfig": {
+                  "@type": "type.googleapis.com/espv2.api.envoy.v11.http.grpc_metadata_scrubber.FilterConfig"
+                }
+              },
+              {
+                "name": "envoy.filters.http.router",
+                "typedConfig": {
+                  "@type": "type.googleapis.com/envoy.extensions.filters.http.router.v3.Router",
+                  "suppressEnvoyHeaders": true
+                }
+              }
+            ],
+            "httpProtocolOptions": {
+              "enableTrailers": true
+            },
+            "localReplyConfig": {
+              "bodyFormat": {
+                "jsonFormat": {
+                  "code": "%RESPONSE_CODE%",
+                  "message": "%LOCAL_REPLY_BODY%"
+                }
+              }
+            },
+            "mergeSlashes": true,
+            "normalizePath": true,
+            "pathWithEscapedSlashesAction": "KEEP_UNCHANGED",
+            "routeConfig": {
+              "name": "local_route",
+              "virtualHosts": [
+                {
+                  "domains": [
+                    "*"
+                  ],
+                  "name": "backend",
+                  "routes": [
+                    {
+                      "decorator": {
+                        "operation": "ingress CreateShelf"
+                      },
+                      "match": {
+                        "headers": [
+                          {
+                            "name": ":method",
+                            "stringMatch": {
+                              "exact": "GET"
+                            }
+                          }
+                        ],
+                        "path": "/CreateShelf"
+                      },
+                      "name": "endpoints.examples.bookstore.Bookstore.CreateShelf",
+                      "route": {
+                        "cluster": "backend-cluster-http.backend.test:8080",
+                        "hostRewriteLiteral": "http.backend.test",
+                        "idleTimeout": "300s",
+                        "retryPolicy": {
+                          "numRetries": 1,
+                          "retryOn": "reset,connect-failure,refused-stream"
+                        },
+                        "timeout": "15s"
+                      }
+                    },
+                    {
+                      "decorator": {
+                        "operation": "ingress CreateShelf"
+                      },
+                      "match": {
+                        "headers": [
+                          {
+                            "name": ":method",
+                            "stringMatch": {
+                              "exact": "GET"
+                            }
+                          }
+                        ],
+                        "path": "/CreateShelf/"
+                      },
+                      "name": "endpoints.examples.bookstore.Bookstore.CreateShelf",
+                      "route": {
+                        "cluster": "backend-cluster-http.backend.test:8080",
+                        "hostRewriteLiteral": "http.backend.test",
+                        "idleTimeout": "300s",
+                        "retryPolicy": {
+                          "numRetries": 1,
+                          "retryOn": "reset,connect-failure,refused-stream"
+                        },
+                        "timeout": "15s"
+                      }
+                    },
+                    {
+                      "decorator": {
+                        "operation": "ingress UnknownHttpMethodForPath_/CreateShelf"
+                      },
+                      "directResponse": {
+                        "body": {
+                          "inlineString": "The current request is matched to the defined url template \"/CreateShelf\" but its http method is not allowed"
+                        },
+                        "status": 405
+                      },
+                      "match": {
+                        "path": "/CreateShelf"
+                      }
+                    },
+                    {
+                      "decorator": {
+                        "operation": "ingress UnknownHttpMethodForPath_/CreateShelf"
+                      },
+                      "directResponse": {
+                        "body": {
+                          "inlineString": "The current request is matched to the defined url template \"/CreateShelf\" but its http method is not allowed"
+                        },
+                        "status": 405
+                      },
+                      "match": {
+                        "path": "/CreateShelf/"
+                      }
+                    },
+                    {
+                      "decorator": {
+                        "operation": "ingress UnknownOperationName"
+                      },
+                      "directResponse": {
+                        "body": {
+                          "inlineString": "The current request is not defined by this API."
+                        },
+                        "status": 404
+                      },
+                      "match": {
+                        "prefix": "/"
+                      }
+                    }
+                  ]
+                }
+              ]
+            },
+            "statPrefix": "ingress_http",
+            "upgradeConfigs": [
+              {
+                "upgradeType": "websocket"
+              }
+            ],
+            "useRemoteAddress": false,
+            "xffNumTrustedHops": 2
+          }
+        }
+      ]
     }
   ],
   "name": "ingress_listener",
