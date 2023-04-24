fb0775c50ca244169b8bd2e768856aa5ede91da7
Teju Nareddy <nareddyt@google.com>
2023-04-24 21:04:38 +0000

    fix breakages
    Signed-off-by: Teju Nareddy <nareddyt@google.com>

--- src/go/configgenerator/clustergen/helpers/tls.go
+++ src/go/configgenerator/clustergen/helpers/tls.go
@@ -2,12 +2,8 @@ package helpers
 
 import (
 	"fmt"
-	"strings"
 
-	"github.com/GoogleCloudPlatform/esp-v2/src/go/util"
 	corepb "github.com/envoyproxy/go-control-plane/envoy/config/core/v3"
-	tlspb "github.com/envoyproxy/go-control-plane/envoy/extensions/transport_sockets/tls/v3"
-	"google.golang.org/protobuf/types/known/anypb"
 )
 
 var (
@@ -33,32 +29,33 @@ func (c *ClusterTLSConfiger) MakeTLSConfig(hostname string, alpnProtocols []stri
 		return nil, fmt.Errorf("root certs path cannot be empty")
 	}
 
-	sslFileName := DefaultClientSslFilename
-	// Backward compatible for ESPv1
-	if strings.Contains(c.ClientCertsPath, "/etc/nginx/ssl") {
-		sslFileName = "backend"
-	}
+	// TODO(nareddyt): Uncomment when util directory change is in PR.
+	//sslFileName := DefaultClientSslFilename
+	//// Backward compatible for ESPv1
+	//if strings.Contains(c.ClientCertsPath, "/etc/nginx/ssl") {
+	//	sslFileName = "backend"
+	//}
+	//
+	//commonTls, err := util.CreateCommonTlsContext(c.RootCertsPath, c.ClientCertsPath, sslFileName, "", "", c.ClientCipherSuites)
+	//if err != nil {
+	//	return nil, err
+	//}
+	//if len(alpnProtocols) > 0 {
+	//	commonTls.AlpnProtocols = alpnProtocols
+	//}
+	//
+	//tlsContext, err := anypb.New(&tlspb.UpstreamTlsContext{
+	//	Sni:              hostname,
+	//	CommonTlsContext: commonTls,
+	//})
+	//if err != nil {
+	//	return nil, err
+	//}
 
-	commonTls, err := util.CreateCommonTlsContext(c.RootCertsPath, c.ClientCertsPath, sslFileName, "", "", c.ClientCipherSuites)
-	if err != nil {
-		return nil, err
-	}
-	if len(alpnProtocols) > 0 {
-		commonTls.AlpnProtocols = alpnProtocols
-	}
-
-	tlsContext, err := anypb.New(&tlspb.UpstreamTlsContext{
-		Sni:              hostname,
-		CommonTlsContext: commonTls,
-	},
-	)
-	if err != nil {
-		return nil, err
-	}
 	return &corepb.TransportSocket{
 		Name: TLSTransportSockerName,
 		ConfigType: &corepb.TransportSocket_TypedConfig{
-			TypedConfig: tlsContext,
+			//TypedConfig: tlsContext,
 		},
 	}, nil
 }
---
+++ src/go/util/dns_resolver.go
@@ -0,0 +1,65 @@
+// Copyright 2020 Google LLC
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+package util
+
+import (
+	"fmt"
+	"strconv"
+	"strings"
+
+	corepb "github.com/envoyproxy/go-control-plane/envoy/config/core/v3"
+)
+
+func DnsResolvers(dnsResolverAddresses string) ([]*corepb.Address, error) {
+	var dnsResolvers []*corepb.Address
+	addressSlice := strings.Split(dnsResolverAddresses, ";")
+	for _, DnsResolverAddress := range addressSlice {
+		host, port, err := parseDnsResolverAddress(DnsResolverAddress)
+		if err != nil {
+			return nil, fmt.Errorf("fail to parse dnsResolverAddress: %v", err)
+		}
+
+		dnsResolvers = append(dnsResolvers, &corepb.Address{
+			Address: &corepb.Address_SocketAddress{
+				SocketAddress: &corepb.SocketAddress{
+					Address: host,
+					PortSpecifier: &corepb.SocketAddress_PortValue{
+						PortValue: port,
+					},
+				},
+			},
+		})
+	}
+
+	return dnsResolvers, nil
+}
+
+func parseDnsResolverAddress(address string) (string, uint32, error) {
+	arr := strings.Split(address, ":")
+	if len(arr) == 0 || len(arr) > 2 {
+		return "", 0, fmt.Errorf("address has a more than one colon: %s", address)
+	}
+
+	if len(arr) == 1 {
+		arr = append(arr, DNSDefaultPort)
+	}
+
+	portVal, err := strconv.Atoi(arr[1])
+	if err != nil {
+		return "", 0, err
+	}
+
+	return arr[0], uint32(portVal), nil
+}
