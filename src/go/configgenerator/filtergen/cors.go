8a01bacbbf575a1f8c140f5135c5627da13ed4d5
Teju Nareddy <nareddyt@google.com>
2023-08-11 12:29:11 -0500

    back to 1 cors gen
    Signed-off-by: Teju Nareddy <nareddyt@google.com>

--- src/go/configgenerator/filtergen/cors.go
+++ src/go/configgenerator/filtergen/cors.go
@@ -33,117 +33,87 @@ const (
 	CORSFilterName = "envoy.filters.http.cors"
 )
 
-type CORSBasicGenerator struct {
-	// AllowOrigin is the name of the origin to allow through.
+// CORSGenerator is a FilterGenerator to configure CORS config.
+type CORSGenerator struct {
+	Preset string
+	// AllowOrigin should only be set if preset=basic
 	AllowOrigin string
-	Options     *CorsOptions
-
-	NoopFilterGenerator
-}
-
-type CORSRegexGenerator struct {
-	// AllowOriginRegex is the regex of the origins to allow through.
+	// AllowOriginRegex should only be set if preset=cors_with_regex
 	AllowOriginRegex string
-	Options          *CorsOptions
-
-	NoopFilterGenerator
-}
-
-type CorsOptions struct {
 	MaxAge           time.Duration
 	AllowMethods     string
 	AllowHeaders     string
 	ExposeHeaders    string
 	AllowCredentials bool
+
+	NoopFilterGenerator
 }
 
 // NewCORSFilterGensFromOPConfig creates a CORSGenerator from
 // OP service config + descriptor + ESPv2 options. It is a FilterGeneratorOPFactory.
 func NewCORSFilterGensFromOPConfig(serviceConfig *servicepb.Service, opts options.ConfigGeneratorOptions) ([]FilterGenerator, error) {
-	corsOptions := &CorsOptions{
-		MaxAge:           opts.CorsMaxAge,
-		AllowMethods:     opts.CorsAllowMethods,
-		AllowHeaders:     opts.CorsAllowHeaders,
-		ExposeHeaders:    opts.CorsExposeHeaders,
-		AllowCredentials: opts.CorsAllowCredentials,
-	}
-
-	switch opts.CorsPreset {
-	case "":
+	if opts.CorsPreset == "" {
 		glog.Infof("Not adding CORS filter gen because the feature is disabled by option, option is currently %q", opts.CorsPreset)
 		return nil, nil
+	}
 
+	return []FilterGenerator{
+		&CORSGenerator{
+			Preset:           opts.CorsPreset,
+			AllowOrigin:      opts.CorsAllowOrigin,
+			AllowOriginRegex: opts.CorsAllowOriginRegex,
+			MaxAge:           opts.CorsMaxAge,
+			AllowMethods:     opts.CorsAllowMethods,
+			AllowHeaders:     opts.CorsAllowHeaders,
+			ExposeHeaders:    opts.CorsExposeHeaders,
+			AllowCredentials: opts.CorsAllowCredentials,
+		},
+	}, nil
+}
+
+func (g *CORSGenerator) FilterName() string {
+	return CORSFilterName
+}
+
+func (g *CORSGenerator) GenFilterConfig() (proto.Message, error) {
+	return &corspb.Cors{}, nil
+}
+
+func (g *CORSGenerator) GenPerHostConfig(vHostName string) (proto.Message, error) {
+	policy := &corspb.CorsPolicy{
+		MaxAge:        strconv.Itoa(int(g.MaxAge.Seconds())),
+		AllowMethods:  g.AllowMethods,
+		AllowHeaders:  g.AllowHeaders,
+		ExposeHeaders: g.ExposeHeaders,
+		AllowCredentials: &wrapperspb.BoolValue{
+			Value: g.AllowCredentials,
+		},
+	}
+
+	switch g.Preset {
 	case "basic":
-		return []FilterGenerator{
-			&CORSBasicGenerator{
-				AllowOrigin: opts.CorsAllowOrigin,
-				Options:     corsOptions,
+		policy.AllowOriginStringMatch = []*matcherpb.StringMatcher{
+			{
+				MatchPattern: &matcherpb.StringMatcher_Exact{
+					Exact: g.AllowOrigin,
+				},
 			},
-		}, nil
+		}
 
 	case "cors_with_regex":
-		return []FilterGenerator{
-			&CORSRegexGenerator{
-				AllowOriginRegex: opts.CorsAllowOriginRegex,
-				Options:          corsOptions,
+		policy.AllowOriginStringMatch = []*matcherpb.StringMatcher{
+			{
+				MatchPattern: &matcherpb.StringMatcher_SafeRegex{
+					SafeRegex: &matcherpb.RegexMatcher{
+						Regex: g.AllowOriginRegex,
+					},
+				},
 			},
-		}, nil
+		}
 
 	default:
 		return nil, fmt.Errorf(`cors_preset must be either "basic" or "cors_with_regex"`)
 	}
-}
 
-func (g *CORSBasicGenerator) FilterName() string {
-	return CORSFilterName
-}
-
-func (g *CORSBasicGenerator) GenFilterConfig() (proto.Message, error) {
-	return &corspb.Cors{}, nil
-}
-
-func (g *CORSBasicGenerator) GenPerHostConfig(vHostName string) (proto.Message, error) {
-	policy := genCorsPolicyFromOptions(g.Options)
-	policy.AllowOriginStringMatch = []*matcherpb.StringMatcher{
-		{
-			MatchPattern: &matcherpb.StringMatcher_Exact{
-				Exact: g.AllowOrigin,
-			},
-		},
-	}
 	return policy, nil
-}
-
-func (g *CORSRegexGenerator) FilterName() string {
-	return CORSFilterName
-}
-
-func (g *CORSRegexGenerator) GenFilterConfig() (proto.Message, error) {
-	return &corspb.Cors{}, nil
-}
-
-func (g *CORSRegexGenerator) GenPerHostConfig(vHostName string) (proto.Message, error) {
-	policy := genCorsPolicyFromOptions(g.Options)
-	policy.AllowOriginStringMatch = []*matcherpb.StringMatcher{
-		{
-			MatchPattern: &matcherpb.StringMatcher_SafeRegex{
-				SafeRegex: &matcherpb.RegexMatcher{
-					Regex: g.AllowOriginRegex,
-				},
-			},
-		},
-	}
-	return policy, nil
-}
-
-func genCorsPolicyFromOptions(options *CorsOptions) *corspb.CorsPolicy {
-	return &corspb.CorsPolicy{
-		MaxAge:        strconv.Itoa(int(options.MaxAge.Seconds())),
-		AllowMethods:  options.AllowMethods,
-		AllowHeaders:  options.AllowHeaders,
-		ExposeHeaders: options.ExposeHeaders,
-		AllowCredentials: &wrapperspb.BoolValue{
-			Value: options.AllowCredentials,
-		},
-	}
 }
