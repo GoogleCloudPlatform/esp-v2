39c62881e7e64a1e9d872e4af975cc87a658ef68
April Dong <siqidong@google.com>
2024-01-31 17:18:25 -0800

    Sync latest changes made to google3

--- src/go/util/httppattern/util.go
+++ src/go/util/httppattern/util.go
@@ -12,18 +12,18 @@ const (
 	optionalTrailingSlashRegex = `\/?`
 )
 
-// Wildcard segment matching any char 1 or unlimited times, except '/'.
+// SingleWildcardReplacementRegex does wildcard segment matching any char 1 or unlimited times, except '/'.
 // If disallowColonInWildcardPathSegment=true, it matches any char except '/' and ':'.
-func singleWildcardReplacementRegex(disallowColonInWildcardPathSegment bool) string {
+func SingleWildcardReplacementRegex(disallowColonInWildcardPathSegment bool) string {
 	if disallowColonInWildcardPathSegment {
 		return `[^\/:]+`
 	}
 	return `[^\/]+`
 }
 
-// Wildcard segment matching any char 0 or unlimited times, except '/'.
+// DoubleWildcardReplacementRegex does wildcard segment matching any char 0 or unlimited times, except '/'.
 // If disallowColonInWildcardPathSegment=true, it matches any char except '/' and ':'.
-func doubleWildcardReplacementRegex(disallowColonInWildcardPathSegment bool) string {
+func DoubleWildcardReplacementRegex(disallowColonInWildcardPathSegment bool) string {
 	if disallowColonInWildcardPathSegment {
 		return `[^:]*`
 	}
