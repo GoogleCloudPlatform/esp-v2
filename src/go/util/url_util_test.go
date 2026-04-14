0e27455edb361949698fdea5f68661b8bc6bedbc
Sunay Dagli <sunaydagli@google.com>
2026-04-14 20:28:20 +0000

    fix go mod

--- src/go/util/url_util_test.go
+++ src/go/util/url_util_test.go
@@ -206,7 +206,7 @@ func TestParseURI(t *testing.T) {
 		{
 			desc:    "fail for too many colons before port",
 			url:     "grpcs://127.0.0.1:::80",
-			wantErr: `too many colons in address`,
+			wantErr: `parse "grpcs://127.0.0.1:::80": `,
 		},
 		{
 			desc:    "bad escape sequence in path",
