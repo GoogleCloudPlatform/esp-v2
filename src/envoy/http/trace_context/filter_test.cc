a9e132817f6c8f7938ed1c7211577f147e5a5c24
Tiger Sun <xunsun@google.com>
2026-09-03 18:28:19 -0700

    test(tracing): Overhaul trace context filter test suite and verify transparent passthrough
    Add unit test verification for Task C from trace context parity analysis:
    - Fix and overhaul filter_test.cc to use current Envoy MockSpan and proto types.
    - Add test coverage for TransparentPassthroughOfStashedTraceparentWhenOutgoingDisabled
      verifying that unprompted Envoy child spans are stripped and client's original
      raw traceparent is restored when TRACE_CONTEXT is omitted from outgoing contexts.
    - Add test cases for stripping unconfigured traceparent when no client header was
      stashed, preserving legacy headers, handling empty/unspecified configs, and
      verifying binary/decimal encoding.
    
    TAG=agy

--- src/envoy/http/trace_context/filter_test.cc
+++ src/envoy/http/trace_context/filter_test.cc
@@ -38,22 +38,23 @@ namespace {
 class TraceContextFilterTest : public ::testing::Test {
  protected:
   void SetUp() override {
-    config_ = std::make_shared<config_pb::TraceContextForwardedConfig>();
-    filter_ = std::make_unique<Filter>(config_);
+    ON_CALL(decoder_callbacks_, activeSpan())
+        .WillByDefault(ReturnRef(active_span_));
+    ON_CALL(active_span_, getTraceId())
+        .WillByDefault(Return("0af7651916cd43dd8448eb211c80319c"));
+    ON_CALL(active_span_, getSpanId())
+        .WillByDefault(Return("b9c7c989f97918e1"));
+  }
+
+  void setupFilter(std::vector<::envoy::v12::http::trace_context::TraceContextFormat> formats) {
+    auto config = std::make_shared<::envoy::v12::http::trace_context::TraceContextForwardedConfig>();
+    for (auto format : formats) {
+      config->add_outgoing_contexts(format);
+    }
+    filter_ = std::make_unique<Filter>(config);
     filter_->setDecoderFilterCallbacks(decoder_callbacks_);
-    
-    ON_CALL(decoder_callbacks_, activeSpan()).WillByDefault(ReturnRef(active_span_));
   }
 
-  void setupCloudTraceFormat() {
-    config_->add_trace_context_format(config_pb::TraceContextConfig::CLOUD_TRACE_CONTEXT);
-  }
-
-  void setupGrpcTraceBinFormat() {
-    config_->add_trace_context_format(config_pb::TraceContextConfig::GRPC_TRACE_BIN);
-  }
-
-  std::shared_ptr<config_pb::TraceContextForwardedConfig> config_;
   std::unique_ptr<Filter> filter_;
   NiceMock<Envoy::Http::MockStreamDecoderFilterCallbacks> decoder_callbacks_;
   NiceMock<Envoy::Tracing::MockSpan> active_span_;
@@ -61,60 +62,236 @@ class TraceContextFilterTest : public ::testing::Test {
 };
 
 TEST_F(TraceContextFilterTest, CloudTraceContextFormat) {
-  setupCloudTraceFormat();
-  
-  EXPECT_CALL(active_span_, injectContext(_, _)).WillOnce(Invoke([](Envoy::Tracing::TraceContext& trace_context, const Envoy::Tracing::UpstreamContext&) {
-    trace_context.setCopy(TraceContextHeadersSingleton::get().Traceparent, "00-0af7651916cd43dd8448eb211c80319c-b9c7c989f97918e1-01");
-  }));
+  setupFilter({::envoy::v12::http::trace_context::CLOUD_TRACE_CONTEXT});
 
-  EXPECT_EQ(Envoy::Http::FilterHeadersStatus::Continue, filter_->decodeHeaders(headers_, false));
+  EXPECT_EQ(Envoy::Http::FilterHeadersStatus::Continue,
+            filter_->decodeHeaders(headers_, false));
 
-  EXPECT_EQ("0af7651916cd43dd8448eb211c80319c/13386927375254264033;o=1", std::string(headers_.get_(TraceContextHeadersSingleton::get().CloudTraceContext.get())));
+  EXPECT_EQ(
+      "0af7651916cd43dd8448eb211c80319c/13386890011815254241;o=1",
+      headers_.get_(TraceContextHeadersSingleton::get().CloudTraceContext.get()));
+  EXPECT_TRUE(
+      headers_.get_(TraceContextHeadersSingleton::get().GrpcTraceBin.get())
+          .empty());
+  EXPECT_TRUE(
+      headers_.get_(TraceContextHeadersSingleton::get().Traceparent.get())
+          .empty());
 }
 
 TEST_F(TraceContextFilterTest, GrpcTraceBinFormat) {
-  setupGrpcTraceBinFormat();
+  setupFilter({::envoy::v12::http::trace_context::GRPC_TRACE_BIN});
 
-  EXPECT_CALL(active_span_, injectContext(_, _)).WillOnce(Invoke([](Envoy::Tracing::TraceContext& trace_context, const Envoy::Tracing::UpstreamContext&) {
-    trace_context.setCopy(TraceContextHeadersSingleton::get().Traceparent, "00-0af7651916cd43dd8448eb211c80319c-b9c7c989f97918e1-01");
-  }));
-
-  EXPECT_EQ(Envoy::Http::FilterHeadersStatus::Continue, filter_->decodeHeaders(headers_, false));
+  EXPECT_EQ(Envoy::Http::FilterHeadersStatus::Continue,
+            filter_->decodeHeaders(headers_, false));
 
   const char kExpectedGrpcTraceBin[] = {
       '\x00', '\x00', '\x0a', '\xf7', '\x65', '\x19', '\x16', '\xcd',
       '\x43', '\xdd', '\x84', '\x48', '\xeb', '\x21', '\x1c', '\x80',
       '\x31', '\x9c', '\x01', '\xb9', '\xc7', '\xc9', '\x89', '\xf9',
-      '\x79', '\x18', '\xe1', '\x02', '\x01'
-  };
+      '\x79', '\x18', '\xe1', '\x02', '\x01'};
   absl::string_view expected_grpc_trace_bin(kExpectedGrpcTraceBin, 29);
-  
-  EXPECT_EQ(expected_grpc_trace_bin, headers_.get_(TraceContextHeadersSingleton::get().GrpcTraceBin.get()));
+
+  // Wire format is base64-encoded
+  EXPECT_EQ(
+      "AAAK92UZFs1D3YRI6yEcgDGcAbnHyYn5eRjhAgE=",
+      headers_.get_(TraceContextHeadersSingleton::get().GrpcTraceBin.get()));
+  std::string decoded_header;
+  ASSERT_TRUE(absl::Base64Unescape(
+      headers_.get_(TraceContextHeadersSingleton::get().GrpcTraceBin.get()),
+      &decoded_header));
+  EXPECT_EQ(expected_grpc_trace_bin, decoded_header);
+
+  EXPECT_TRUE(
+      headers_.get_(TraceContextHeadersSingleton::get().CloudTraceContext.get())
+          .empty());
 }
 
-TEST_F(TraceContextFilterTest, FormatUnspecified) {
-  config_->add_trace_context_format(config_pb::TraceContextConfig::TRACE_CONTEXT_FORMAT_UNSPECIFIED);
+TEST_F(TraceContextFilterTest, TraceContextFormatPreservesEnvoyChildSpan) {
+  setupFilter({::envoy::v12::http::trace_context::TRACE_CONTEXT});
 
-  EXPECT_CALL(active_span_, injectContext(_, _)).WillOnce(Invoke([](Envoy::Tracing::TraceContext& trace_context, const Envoy::Tracing::UpstreamContext&) {
-    trace_context.setCopy(TraceContextHeadersSingleton::get().Traceparent, "00-0af7651916cd43dd8448eb211c80319c-b9c7c989f97918e1-01");
-  }));
+  // Envoy tracer wrote its child span
+  headers_.addCopy(
+      TraceContextHeadersSingleton::get().Traceparent,
+      "00-0af7651916cd43dd8448eb211c80319c-b9c7c989f97918e1-01");
+  // Stashed original client header
+  headers_.addCopy(
+      TraceContextHeadersSingleton::get().OriginalTraceparent,
+      "00-clientraw123456789012345678901234-0000000000000002-01");
 
-  EXPECT_EQ(Envoy::Http::FilterHeadersStatus::Continue, filter_->decodeHeaders(headers_, false));
+  EXPECT_EQ(Envoy::Http::FilterHeadersStatus::Continue,
+            filter_->decodeHeaders(headers_, false));
 
-  EXPECT_TRUE(headers_.get_(TraceContextHeadersSingleton::get().CloudTraceContext.get()).empty());
-  EXPECT_TRUE(headers_.get_(TraceContextHeadersSingleton::get().GrpcTraceBin.get()).empty());
+  // Child span preserved because TRACE_CONTEXT was configured
+  EXPECT_EQ(
+      "00-0af7651916cd43dd8448eb211c80319c-b9c7c989f97918e1-01",
+      headers_.get_(TraceContextHeadersSingleton::get().Traceparent.get()));
+  // Stash header scrubbed
+  EXPECT_TRUE(headers_
+                  .get_(TraceContextHeadersSingleton::get()
+                            .OriginalTraceparent.get())
+                  .empty());
+}
+
+TEST_F(TraceContextFilterTest,
+       TransparentPassthroughOfStashedTraceparentWhenOutgoingDisabled) {
+  // Outgoing tracing does not include TRACE_CONTEXT
+  setupFilter({::envoy::v12::http::trace_context::CLOUD_TRACE_CONTEXT});
+
+  // Envoy tracer added an unprompted child span
+  headers_.addCopy(
+      TraceContextHeadersSingleton::get().Traceparent,
+      "00-envoytraceid999999999999999999999-envoychildspan1-01");
+  // Ingress filter stashed the client's unconfigured raw traceparent
+  headers_.addCopy(
+      TraceContextHeadersSingleton::get().OriginalTraceparent,
+      "00-clientraw123456789012345678901234-0000000000000002-01");
+
+  EXPECT_EQ(Envoy::Http::FilterHeadersStatus::Continue,
+            filter_->decodeHeaders(headers_, false));
+
+  // Unprompted Envoy child span is stripped and client's original traceparent is restored
+  EXPECT_EQ(
+      "00-clientraw123456789012345678901234-0000000000000002-01",
+      headers_.get_(TraceContextHeadersSingleton::get().Traceparent.get()));
+  // Stash header scrubbed
+  EXPECT_TRUE(headers_
+                  .get_(TraceContextHeadersSingleton::get()
+                            .OriginalTraceparent.get())
+                  .empty());
+  // Cloud trace header is generated
+  EXPECT_EQ(
+      "0af7651916cd43dd8448eb211c80319c/13386890011815254241;o=1",
+      headers_.get_(TraceContextHeadersSingleton::get().CloudTraceContext.get()));
+}
+
+TEST_F(TraceContextFilterTest, StripsTraceparentWhenOutgoingDisabledAndNoStashed) {
+  setupFilter({::envoy::v12::http::trace_context::CLOUD_TRACE_CONTEXT});
+
+  // Envoy tracer added child span, but there was no stashed traceparent
+  headers_.addCopy(
+      TraceContextHeadersSingleton::get().Traceparent,
+      "00-envoytraceid999999999999999999999-envoychildspan1-01");
+
+  EXPECT_EQ(Envoy::Http::FilterHeadersStatus::Continue,
+            filter_->decodeHeaders(headers_, false));
+
+  // Traceparent stripped completely
+  EXPECT_TRUE(
+      headers_.get_(TraceContextHeadersSingleton::get().Traceparent.get())
+          .empty());
+  EXPECT_TRUE(headers_
+                  .get_(TraceContextHeadersSingleton::get()
+                            .OriginalTraceparent.get())
+                  .empty());
+}
+
+TEST_F(TraceContextFilterTest, PreservesLegacyHeadersWhenOutgoingDisabled) {
+  // Outgoing configured only for TRACE_CONTEXT
+  setupFilter({::envoy::v12::http::trace_context::TRACE_CONTEXT});
+
+  // Client legacy headers present on the stream
+  headers_.addCopy(TraceContextHeadersSingleton::get().CloudTraceContext,
+                   "105445aa7843bc8bf206b12000100000/1;o=1");
+  headers_.addCopy(TraceContextHeadersSingleton::get().GrpcTraceBin,
+                   "dummy-grpc-trace-bin");
+
+  EXPECT_EQ(Envoy::Http::FilterHeadersStatus::Continue,
+            filter_->decodeHeaders(headers_, false));
+
+  // Legacy headers not touched by filter
+  EXPECT_EQ(
+      "105445aa7843bc8bf206b12000100000/1;o=1",
+      headers_.get_(TraceContextHeadersSingleton::get().CloudTraceContext.get()));
+  EXPECT_EQ(
+      "dummy-grpc-trace-bin",
+      headers_.get_(TraceContextHeadersSingleton::get().GrpcTraceBin.get()));
+}
+
+TEST_F(TraceContextFilterTest, MultipleOutgoingContexts) {
+  setupFilter({::envoy::v12::http::trace_context::CLOUD_TRACE_CONTEXT,
+               ::envoy::v12::http::trace_context::GRPC_TRACE_BIN,
+               ::envoy::v12::http::trace_context::TRACE_CONTEXT});
+
+  headers_.addCopy(
+      TraceContextHeadersSingleton::get().Traceparent,
+      "00-0af7651916cd43dd8448eb211c80319c-b9c7c989f97918e1-01");
+
+  EXPECT_EQ(Envoy::Http::FilterHeadersStatus::Continue,
+            filter_->decodeHeaders(headers_, false));
+
+  EXPECT_EQ(
+      "0af7651916cd43dd8448eb211c80319c/13386890011815254241;o=1",
+      headers_.get_(TraceContextHeadersSingleton::get().CloudTraceContext.get()));
+  EXPECT_EQ(
+      "AAAK92UZFs1D3YRI6yEcgDGcAbnHyYn5eRjhAgE=",
+      headers_.get_(TraceContextHeadersSingleton::get().GrpcTraceBin.get()));
+  EXPECT_EQ(
+      "00-0af7651916cd43dd8448eb211c80319c-b9c7c989f97918e1-01",
+      headers_.get_(TraceContextHeadersSingleton::get().Traceparent.get()));
 }
 
 TEST_F(TraceContextFilterTest, FormatEmpty) {
-  // Empty configuration should just no-op
-  EXPECT_CALL(active_span_, injectContext(_, _)).WillOnce(Invoke([](Envoy::Tracing::TraceContext& trace_context, const Envoy::Tracing::UpstreamContext&) {
-    trace_context.setCopy(TraceContextHeadersSingleton::get().Traceparent, "00-0af7651916cd43dd8448eb211c80319c-b9c7c989f97918e1-01");
-  }));
+  setupFilter({});
 
-  EXPECT_EQ(Envoy::Http::FilterHeadersStatus::Continue, filter_->decodeHeaders(headers_, false));
+  headers_.addCopy(
+      TraceContextHeadersSingleton::get().Traceparent,
+      "00-envoytraceid999999999999999999999-envoychildspan1-01");
+  headers_.addCopy(
+      TraceContextHeadersSingleton::get().OriginalTraceparent,
+      "00-clientraw123456789012345678901234-0000000000000002-01");
 
-  EXPECT_TRUE(headers_.get_(TraceContextHeadersSingleton::get().CloudTraceContext.get()).empty());
-  EXPECT_TRUE(headers_.get_(TraceContextHeadersSingleton::get().GrpcTraceBin.get()).empty());
+  EXPECT_EQ(Envoy::Http::FilterHeadersStatus::Continue,
+            filter_->decodeHeaders(headers_, false));
+
+  // Restores stashed traceparent, does not create any legacy headers
+  EXPECT_EQ(
+      "00-clientraw123456789012345678901234-0000000000000002-01",
+      headers_.get_(TraceContextHeadersSingleton::get().Traceparent.get()));
+  EXPECT_TRUE(
+      headers_.get_(TraceContextHeadersSingleton::get().CloudTraceContext.get())
+          .empty());
+  EXPECT_TRUE(
+      headers_.get_(TraceContextHeadersSingleton::get().GrpcTraceBin.get())
+          .empty());
+  EXPECT_TRUE(headers_
+                  .get_(TraceContextHeadersSingleton::get()
+                            .OriginalTraceparent.get())
+                  .empty());
+}
+
+TEST_F(TraceContextFilterTest, FormatUnspecified) {
+  setupFilter({::envoy::v12::http::trace_context::TRACE_CONTEXT_FORMAT_UNSPECIFIED});
+
+  EXPECT_EQ(Envoy::Http::FilterHeadersStatus::Continue,
+            filter_->decodeHeaders(headers_, false));
+
+  EXPECT_TRUE(
+      headers_.get_(TraceContextHeadersSingleton::get().CloudTraceContext.get())
+          .empty());
+  EXPECT_TRUE(
+      headers_.get_(TraceContextHeadersSingleton::get().GrpcTraceBin.get())
+          .empty());
+}
+
+TEST_F(TraceContextFilterTest, ActiveSpanEmptyDoesNotCrash) {
+  setupFilter({::envoy::v12::http::trace_context::CLOUD_TRACE_CONTEXT});
+
+  EXPECT_CALL(active_span_, getTraceId()).WillRepeatedly(Return(""));
+  EXPECT_CALL(active_span_, getSpanId()).WillRepeatedly(Return(""));
+
+  EXPECT_EQ(Envoy::Http::FilterHeadersStatus::Continue,
+            filter_->decodeHeaders(headers_, false));
+
+  EXPECT_TRUE(
+      headers_.get_(TraceContextHeadersSingleton::get().CloudTraceContext.get())
+          .empty());
+}
+
+TEST_F(TraceContextFilterTest, EncodeHeadersContinues) {
+  setupFilter({});
+  Envoy::Http::TestResponseHeaderMapImpl resp_headers;
+  EXPECT_EQ(Envoy::Http::FilterHeadersStatus::Continue,
+            filter_->encodeHeaders(resp_headers, false));
 }
 
 }  // namespace
